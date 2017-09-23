2a7af599c2b3b7386e2da6f55a35a8cc00484c99
Max Lynch <max@drifty.com>
2017-09-23 17:38:55 -0500

    Site menu

--- src/components/site-menu/site-menu.tsx
+++ src/components/site-menu/site-menu.tsx
@@ -11,102 +11,104 @@ export class SiteMenu {
         <stencil-route-link url="/" class="logo-link">
           <img class="logo" alt="Ionic" src="/assets/img/text-logo.png" />
         </stencil-route-link>
-        <ul>
-          <li>
-            <h4>Introduction</h4>
-            <ul>
-              <li>
-                <stencil-route-link url="/docs/intro">
-                  What is Stencil?
-                </stencil-route-link>
-              </li>
-              <li>
-                <stencil-route-link url="/docs/getting-started">
-                  Getting Started
-                </stencil-route-link>
-              </li>
-            </ul>
-          </li>
+        <nav>
+          <ul>
+            <li>
+              <h4>Introduction</h4>
+              <ul>
+                <li>
+                  <stencil-route-link url="/docs/intro">
+                    What is Stencil?
+                  </stencil-route-link>
+                </li>
+                <li>
+                  <stencil-route-link url="/docs/getting-started">
+                    Getting Started
+                  </stencil-route-link>
+                </li>
+              </ul>
+            </li>
 
-          <li>
-            <h4>Basics</h4>
-            <ul>
+            <li>
+              <h4>Basics</h4>
+              <ul>
 
-              <li>
-                <stencil-route-link url="/docs/my-first-component">
-                  My First Component
-                </stencil-route-link>
-              </li>
+                <li>
+                  <stencil-route-link url="/docs/my-first-component">
+                    My First Component
+                  </stencil-route-link>
+                </li>
 
-              <li>
-                <stencil-route-link url="/docs/templating">
-                  Templating
-                </stencil-route-link>
-              </li>
+                <li>
+                  <stencil-route-link url="/docs/templating">
+                    Templating
+                  </stencil-route-link>
+                </li>
 
-              <li>
-                <stencil-route-link url="/docs/decorators">
-                  Decorators
-                </stencil-route-link>
-              </li>
+                <li>
+                  <stencil-route-link url="/docs/decorators">
+                    Decorators
+                  </stencil-route-link>
+                </li>
 
-              <li>
-                <stencil-route-link url="/docs/events">
-                  Events
-                </stencil-route-link>
-              </li>
+                <li>
+                  <stencil-route-link url="/docs/events">
+                    Events
+                  </stencil-route-link>
+                </li>
 
-              <li>
-                <stencil-route-link url="/docs/component-lifecycle">
-                  Component Life Cycle
-                </stencil-route-link>
-              </li>
+                <li>
+                  <stencil-route-link url="/docs/component-lifecycle">
+                    Component Life Cycle
+                  </stencil-route-link>
+                </li>
 
-            </ul>
-          </li>
+              </ul>
+            </li>
 
-          <li>
-            <h4>Advanced</h4>
-            <ul>
-              <li>
-                <stencil-route-link url="/docs/stencil-config">
-                  Stencil Configuration
-                </stencil-route-link>
-              </li>
-              <li>
-                <stencil-route-link url="/docs/prerendering">
-                  Prerendering
-                </stencil-route-link>
-              </li>
-              <li>
-                <stencil-route-link url="/docs/server-side-rendering">
-                  Server Side Rendering
-                </stencil-route-link>
-              </li>
-              <li>
-                <stencil-route-link url="/docs/service-workers">
-                  Service Workers
-                </stencil-route-link>
-              </li>
-              <li>
-                <stencil-route-link url="/docs/distribution">
-                  Distribution
-                </stencil-route-link>
-              </li>
-            </ul>
-          </li>
+            <li>
+              <h4>Advanced</h4>
+              <ul>
+                <li>
+                  <stencil-route-link url="/docs/stencil-config">
+                    Stencil Configuration
+                  </stencil-route-link>
+                </li>
+                <li>
+                  <stencil-route-link url="/docs/prerendering">
+                    Prerendering
+                  </stencil-route-link>
+                </li>
+                <li>
+                  <stencil-route-link url="/docs/server-side-rendering">
+                    Server Side Rendering
+                  </stencil-route-link>
+                </li>
+                <li>
+                  <stencil-route-link url="/docs/service-workers">
+                    Service Workers
+                  </stencil-route-link>
+                </li>
+                <li>
+                  <stencil-route-link url="/docs/distribution">
+                    Distribution
+                  </stencil-route-link>
+                </li>
+              </ul>
+            </li>
 
-          <li>
-            <h4>Add-ons</h4>
-            <ul>
-              <li>
-                <stencil-route-link url="/docs/routing">
-                  Router
-                </stencil-route-link>
-              </li>
-            </ul>
-          </li>
-        </ul>
+            <li>
+              <h4>Add-ons</h4>
+              <ul>
+                <li>
+                  <stencil-route-link url="/docs/routing">
+                    Router
+                  </stencil-route-link>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </nav>
       </div>
     );
   }
