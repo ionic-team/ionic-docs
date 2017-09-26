8cc7d1b144455a3f11ce0f34b8cf2be34a159e34
Max Lynch <max@drifty.com>
2017-09-26 17:22:57 -0500

    Expander

--- src/components/menu-expander/menu-expander.tsx
+++ src/components/menu-expander/menu-expander.tsx
@@ -1,17 +1,36 @@
-import { Component, Prop } from '@stencil/core';
+import { Component, Prop, State, Element } from '@stencil/core';
 
 @Component({
   tag: 'menu-expander',
   styleUrl: 'menu-expander.scss'
 })
 export class MenuExpander {
+  @Element() el: HTMLElement;
+
   @Prop() service: string;
+
+  @State() isExpanded: boolean = false;
+
+  target: Element;
+
+  componentDidLoad() {
+    const ul = this.el.nextElementSibling;
+    console.log('Found our ul child', ul);
+    this.target = ul;
+  }
+
+  handleOnClick(e) {
+    if(!this.target) { return; }
+
+    this.target.classList.toggle('expanded');
+  }
 
   render() {
     return (
-    <div>
-      <div class={`pro-icon pro-icon-${this.service}`}></div>
-      <a><slot /></a>
-    </div>)
+      <div onClick={this.handleOnClick.bind(this)}>
+        <div class={`pro-icon pro-icon-${this.service}`}></div>
+        <a><slot /></a>
+      </div>
+    )
   }
 }
