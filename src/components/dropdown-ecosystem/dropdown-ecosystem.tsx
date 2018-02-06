9f2737009003df5e9f789981f3fb93edfd1953fd
perry <perry@ionic.io>
2018-02-06 00:12:25 -0600

    ecosystem dropdown basics

--- src/components/dropdown-ecosystem/dropdown-ecosystem.tsx
+++ src/components/dropdown-ecosystem/dropdown-ecosystem.tsx
@@ -25,7 +25,27 @@ export class DropdownFramework {
     return [
       <a class="current" onClick={this.toggle}>Ecosystem</a>,
       <ul class={dropdownClass} onMouseLeave={this.clear}>
-
+        <li class="forum">
+          <a href="https://forum.ionicframework.com/" target="_blank">Forum</a>
+        </li>
+        <li class="chat">
+          <a href="https://ionicworldwide.herokuapp.com/"
+             target="_blank">Chat</a>
+        </li>
+        <li class="blog">
+          <a href="https://blog.ionicframework.com/" target="_blank">Blog</a>
+        </li>
+        <li class="twitter">
+          <a href="https://twitter.com/Ionicframework"
+             target="_blank">Twitter</a>
+        </li>
+        <li class="stack-overflow">
+          <a href="https://stackoverflow.com/questions/tagged/ionic-framework"
+             target="_blank">Stack</a>
+        </li>
+        <li class="swag">
+          <a href="https://shop.ionicframework.com/" target="_blank">Swag</a>
+        </li>
       </ul>
     ]
   }
