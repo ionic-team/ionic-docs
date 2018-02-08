cdadf26119759a07487e64869fbda730869ef25a
Cam Wiegert <cam@camwiegert.com>
2018-02-08 11:05:24 -0600

    chore() remove legacy files/packages/scripts

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var markdown = require('gulp-markdown');
-var hljs = require('highlight.js');
-gulp.task('default', function() {
-  return gulp
-    .src('./docs-md/**/*.md')
-    .pipe(gulp.dest('./www/docs-content'));
-});
