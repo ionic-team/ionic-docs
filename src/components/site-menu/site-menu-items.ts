e964facf936bed67a42bde490738be799fc4fd1a
Cam Wiegert <cam@camwiegert.com>
2018-02-07 20:26:44 -0600

    refactor(site-menu) build menu from object (#1)

---
+++ src/components/site-menu/site-menu-items.ts
@@ -0,0 +1,26 @@
+export default {
+  'Home': '/',
+  'Introduction': {
+    'What is Ionic Framework': '/',
+    'Basic Features': '/intro/basics',
+    'App Structure': '/intro/structure',
+    'Core Concepts': '/intro/concepts',
+    'Browser Support': '/intro/support'
+  },
+  'Getting Started': {
+    'Installation': '/getting-started/installation',
+    'Starting an App': '/getting-started/starting-an-app',
+    'Run Your App': '/getting-started/run-your-app',
+    'Dev Build': '/getting-started/dev-build',
+    'Testing': '/getting-started/testing',
+    'Deploying': '/getting-started/deploying',
+    'Releasing': '/getting-started/releasing',
+    'Tutorial': '/getting-started/tutorial'
+  },
+  'Components': {},
+  'API Reference': {},
+  'Native APIs': {},
+  'Theming': {},
+  'Contributing': {},
+  'FAQ': {}
+};
