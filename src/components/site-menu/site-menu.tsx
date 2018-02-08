014c1f14406adf977bbd4a48bf9f4cd3dd51e1d3
Cam Wiegert <cam@camwiegert.com>
2018-02-08 13:23:44 -0600

    feat(site-menu) accept functions as menu map values

--- src/components/site-menu/site-menu.tsx
+++ src/components/site-menu/site-menu.tsx
@@ -1,4 +1,4 @@
-import { Component } from '@stencil/core';
+import { Component, State } from '@stencil/core';
 import menuItems from './site-menu-items';
 
 @Component({
@@ -6,12 +6,20 @@ import menuItems from './site-menu-items';
   styleUrl: 'site-menu.scss'
 })
 export class SiteMenu {
+  @State() version: string = '4.0.0';
+
   createMenu(items) {
-    return Object.keys(items).map(key =>
-      typeof items[key] === 'object'
-        ? this.createSubmenu(key, items[key])
-        : this.createMenuItem(key, items[key])
-    );
+    return Object.keys(items).map(key => {
+      const val = items[key];
+      switch (typeof val) {
+        case 'object':
+          return this.createSubmenu(key, val);
+        case 'function':
+          return this.createSubmenu(key, val(this.version));
+        default:
+          return this.createMenuItem(key, val);
+      }
+    });
   }
 
   createMenuItem(text: string, url: string) {
