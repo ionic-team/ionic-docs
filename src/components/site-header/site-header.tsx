b91d4d9f0f6d471971b8992042794447f276ac7f
Cam Wiegert <cam@camwiegert.com>
2018-02-15 14:41:13 -0600

    feat: use svg for new tab icon

--- src/components/site-header/site-header.tsx
+++ src/components/site-header/site-header.tsx
@@ -14,10 +14,17 @@ export class SiteHeader {
         <FrameworkDropdown/>
         <site-search></site-search>
         <EcosystemDropdown/>
-        <a href="https://github.com/ionic-team/ionic"
-           target="_blank"
-           class="github">GitHub</a>
+        <a href="https://github.com/ionic-team/ionic" target="_blank" class="github">
+          GitHub <NewTabIcon/>
+        </a>
       </nav>
     ]
   }
 }
+
+const NewTabIcon = () => (
+  <svg class="new-tab" viewBox="0 0 43 42">
+    <rect class="new-tab__box" y="8" width="34" height="34" rx="6"/>
+    <path class="new-tab__arrow" d="M37.078 3.268H23.617V.243h18.626v18.625h-3.026V5.407L16.13 28.494l-2.14-2.139z"/>
+  </svg>
+);
