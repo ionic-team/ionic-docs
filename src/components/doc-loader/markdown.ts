505e2d3b2ae3061bf95e5ff13fb3deed4c1e1771
Cam Wiegert <cam@camwiegert.com>
2018-02-19 19:53:52 -0600

    feat(doc-loader): add custom heading renderer

--- src/components/doc-loader/markdown.ts
+++ src/components/doc-loader/markdown.ts
@@ -2,9 +2,16 @@ import marked from 'marked';
 import hljs from 'highlight.js';
 
 const renderer = new marked.Renderer();
+const slugify = (text: string) => text.toLowerCase().replace(/[^\w]+/g, '-');
 
 renderer.code = (code: string, lang: string) => `
   <code-block language="${lang}">${hljs.highlightAuto(code).value}</code-block>
+`;
+
+renderer.heading = (text: string, level: string) => `
+  <h${level}>
+    <a class="linked-heading" href="#${slugify(text)}" id="${slugify(text)}">${text}</a>
+  </h${level}>
 `;
 
 export function renderMarkdown(markdown: string): string {
