18a5f45e4ef40ce3edd3fc11c61e7cbee7212ddb
perry <perry@ionic.io>
2018-03-05 12:33:18 -0600

    layout tweaks

--- src/components/site-menu/get-api-ref-map.ts
+++ src/components/site-menu/get-api-ref-map.ts
@@ -1,7 +1,5 @@
 import * as versions from './api-menu';
 
 export default (version) => {
-  console.log(version, versions)
-  console.log(versions.data[version])
   return versions.data[version];
 };
