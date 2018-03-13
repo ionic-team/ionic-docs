fc938f31d2754320d2c53ff9a0cc6fbfe1e927b7
perry <perry@ionic.io>
2018-03-13 10:10:51 -0500

    stencil version bump

--- stencil.config.js
+++ stencil.config.js
@@ -3,7 +3,12 @@ const sass = require('@stencil/sass');
 exports.config = {
   plugins: [sass()],
   copy: [{ src: 'docs-content' }],
-  wwwDir: 'www/docs',
+  outputTargets: [
+    {
+      type: 'www',
+      dir: 'www/docs'
+    }
+  ]
 };
 
 exports.devServer = {
