014fe186a2fe222267611ba0215cb47ed071ac62
perry <perry@ionic.io>
2018-03-13 14:44:57 -0500

    reverting to stencil `0.6.x` for now

--- src/components.d.ts
+++ src/components.d.ts
@@ -13,8 +13,6 @@ declare global {
   interface HTMLStencilElement extends HTMLElement {
     componentOnReady(): Promise<this>;
     componentOnReady(done: (ele?: this) => void): void;
-
-    forceUpdate(): void;
   }
 
   interface HTMLAttributes {}
--- stencil.config.js
+++ stencil.config.js
@@ -3,6 +3,7 @@ const sass = require('@stencil/sass');
 exports.config = {
   plugins: [sass()],
   copy: [{ src: 'docs-content' }],
+  wwwDir: 'www/docs',
   outputTargets: [
     {
       type: 'www',
