4a614c2d13b91d6b6d1c2cca6d1d1bfa437e12f8
perry <perry@ionic.io>
2018-04-18 10:23:39 -0500

    config tidy up

--- scripts/config.ts
+++ scripts/config.ts
@@ -2,14 +2,18 @@
 export const IONIC_ANGULAR_SRC = process.env.ANGULAR_SRC || 'angular/src';
 export const IONIC_CORE_SRC = process.env.CORE_SRC || 'core/src';
 export const IONIC_DIR = process.env.IONIC_DIR || 'sources/ionic';
-export const IONIC_REPO_URL = process.env.MONOREPO_URL || 'https://github.com/ionic-team/ionic';
+export const IONIC_REPO_URL = process.env.IONIC_REPO_URL || 'https://github.com/ionic-team/ionic';
 export const API_DOCS_DIR = process.env.API_DOCS_DIR || 'src/docs-content/api';
 
 export const CLI_DIR = process.env.CLI_DIR || 'sources/ionic-cli';
-export const CLI_REPO_URL = process.env.MONOREPO_URL || 'https://github.com/ionic-team/ionic-cli.git';
+export const CLI_REPO_URL = process.env.CLI_REPO_URL || 'https://github.com/ionic-team/ionic-cli.git';
 export const CLI_DOCS_DIR = process.env.CLI_DOCS_DIR || 'src/docs-content/cli';
 
-export const VERBOSE = bool(process.env.VERBOSE) || false;
+export const NATIVE_DIR = process.env.NATIVE_DIR || 'sources/ionic-native';
+export const NATIVE_REPO_URL = process.env.NATIVE_REPO_URL || 'https://github.com/ionic-team/ionic-native.git';
+export const NATIVE_DOCS_DIR = process.env.NATIVE_DOCS_DIR || 'src/docs-content/native';
+
+export const VERBOSE = bool(process.env.VERBOSE) || true;
 
 
 function bool(str) {
