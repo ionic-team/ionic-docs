8f9e65a6bad458ede3767da2ffdfd5ab6f9682f2
Cam Wiegert <cam@camwiegert.com>
2018-04-23 22:50:49 -0500

    Add base menu structure and styles

---
+++ src/components/docs-menu/docs-menu-map.ts
@@ -0,0 +1,26 @@
+export default {
+  'Home': '/docs',
+  'Introduction': {
+    'What is Ionic Framework': '/docs/intro/intro',
+    'Release Notes': '/docs/intro/release-notes',
+    'Core Concepts': '/docs/intro/concepts',
+    'Browser Support': '/docs/intro/browsers'
+  },
+  'Getting Started': {
+    'Installation': '/docs/getting-started/installation',
+    'Starting an App': '/docs/getting-started/starting-an-app',
+    'Run Your App': '/docs/getting-started/run-your-app',
+    'Dev Build': '/docs/getting-started/dev-build',
+    'Testing': '/docs/getting-started/testing',
+    'Deploying': '/docs/getting-started/deploying',
+    'Releasing': '/docs/getting-started/releasing',
+  },
+  'Components': {},
+  'Theming': {
+        'Basics': '/docs/theming/basics',
+        'Theming with CSS': '/docs/theming/theming-with-css',
+        'Theming with Sass': '/docs/theming/theming-with-sass'
+  },
+  'Contributing': {},
+  'FAQ': {}
+};
--- src/components/docs-menu/docs-menu.tsx
+++ src/components/docs-menu/docs-menu.tsx
@@ -1,14 +1,49 @@
 import { Component } from '@stencil/core';
+import menuMap from './docs-menu-map';
 
 @Component({
   tag: 'docs-menu',
   styleUrl: 'docs-menu.scss'
 })
 export class DocsMenu {
+
+  createItem = (text, url) => {
+    return (
+      <li>
+        <stencil-route-link
+          class="menu__item"
+          activeClass="menu__item--active"
+          url={url}
+          exact>
+            { text }
+        </stencil-route-link>
+      </li>
+    );
+  }
+
+  createSubmenu = (text, items) => {
+    return (
+      <li>
+        <a class="menu__item">{ text }</a>
+        <ul class="menu__submenu">
+          { Object.keys(items).map(key => this.createItem(key, items[key]))}
+        </ul>
+      </li>
+    );
+  }
+
   render() {
     return [
       <div class="section-switch">Framework</div>,
-      <nav class="menu">Menu</nav>
+      <nav class="menu">
+        <ul>
+          { Object.keys(menuMap).map(key => {
+            const val = menuMap[key];
+            const renderer = typeof val === 'string' ? this.createItem : this.createSubmenu;
+            return renderer(key, val);
+          })}
+        </ul>
+      </nav>
     ];
   }
 }
