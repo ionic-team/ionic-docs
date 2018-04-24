3bd644f29183996dadde295102b3b1d044e94355
Cam Wiegert <cam@camwiegert.com>
2018-04-24 10:41:56 -0500

    Add activeItem state to menu

--- src/components/docs-menu/docs-menu.tsx
+++ src/components/docs-menu/docs-menu.tsx
@@ -1,4 +1,4 @@
-import { Component } from '@stencil/core';
+import { Component, State } from '@stencil/core';
 import menuMap from './docs-menu-map';
 
 @Component({
@@ -6,12 +6,14 @@ import menuMap from './docs-menu-map';
   styleUrl: 'docs-menu.scss'
 })
 export class DocsMenu {
+  @State() activeItem: string;
 
-  createItem = (text, url) => {
+  createItem = (text, url, onClick?) => {
     return (
       <li>
         <stencil-route-link
           class="menu__item"
+          onClick={onClick}
           activeClass="menu__item--active"
           url={url}
           exact>
@@ -21,15 +23,19 @@ export class DocsMenu {
     );
   }
 
-  createSubmenu = (text, items) => {
+  createSubmenu = (text, items, onClick?) => {
     return (
       <li>
-        <a class="menu__item">{ text }</a>
-        <ul class="menu__submenu">
+        <a class="menu__item" onClick={onClick}>{ text }</a>
+        <ul class={{ 'menu__submenu': true, 'is-open': this.activeItem === text }}>
           { Object.keys(items).map(key => this.createItem(key, items[key]))}
         </ul>
       </li>
     );
+  }
+
+  setActiveItem(item) {
+    this.activeItem = item;
   }
 
   render() {
@@ -39,8 +45,9 @@ export class DocsMenu {
         <ul>
           { Object.keys(menuMap).map(key => {
             const val = menuMap[key];
+            const onClick = () => this.setActiveItem(key);
             const renderer = typeof val === 'string' ? this.createItem : this.createSubmenu;
-            return renderer(key, val);
+            return renderer(key, val, onClick);
           })}
         </ul>
       </nav>
