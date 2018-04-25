2c90f2110127889eb2e548e13420720718b26986
Cam Wiegert <cam@camwiegert.com>
2018-04-25 08:51:21 -0500

    Add docs-preview from site-preview-app

--- src/components/docs-preview/docs-preview.tsx
+++ src/components/docs-preview/docs-preview.tsx
@@ -1,21 +1,37 @@
-import { Component, Prop } from '@stencil/core';
+import { Component, Prop, State } from '@stencil/core';
 
 @Component({
   tag: 'docs-preview',
   styleUrl: 'docs-preview.scss'
 })
-export class DocsPreview {
+export class SitePreviewApp {
   @Prop() url: string;
+  @State() selected = 'ios';
+  @State() fixed = false;
 
-  hostData() {
-    return {
-      'class': {
-        'is-active': typeof this.url === 'string'
-      }
-    };
+  select(platform) {
+    this.selected = platform;
   }
 
   render() {
-    return <div>{ this.url }</div>;
+    if (! this.url) {
+      return null;
+    }
+
+    return [
+      <ul>
+        <li
+          class={this.selected === 'ios' ? 'active' : ''}
+          onClick={this.select.bind(this, 'ios')}>iOS</li>
+        <li
+          class={this.selected === 'md' ? 'active' : ''}
+          onClick={this.select.bind(this, 'md')}>Android</li>
+      </ul>,
+      <figure class={this.selected}>
+        <iframe
+          src={`${this.url}?ionicplatform=${this.selected}`}
+          frameborder="0"></iframe>
+      </figure>
+    ];
   }
 }
