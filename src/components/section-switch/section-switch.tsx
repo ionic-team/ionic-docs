c1d2baaeacb0b3854c5f42903a1187e715d200bc
Cam Wiegert <cam@camwiegert.com>
2018-04-26 14:42:38 -0500

    Add section-switch selector styles

--- src/components/section-switch/section-switch.tsx
+++ src/components/section-switch/section-switch.tsx
@@ -1,4 +1,5 @@
 import { Component, Prop } from '@stencil/core';
+import { Ionic } from '../../icons';
 
 @Component({
   tag: 'section-switch',
@@ -8,6 +9,17 @@ export class SectionSwitch {
   @Prop() section: string;
 
   render() {
-    return <div>{ this.section }</div>;
+    return [
+      <div class="section-switch__selector">
+        <a class="section-switch__logo" href="/docs">
+          <Ionic/>
+          <span>Docs</span>
+        </a>
+        <span class="section-switch__current">
+          { this.section }
+          <svg viewBox="0 0 33 22"><polygon points="16.5 22 0 0 33 0"></polygon></svg>
+        </span>
+      </div>
+    ];
   }
 }
