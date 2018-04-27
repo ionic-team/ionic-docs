c1728542cb0eb5d11a9c16f424e3f9d702f2da2d
Cam Wiegert <cam@camwiegert.com>
2018-04-27 11:17:50 -0500

    Add section-switch dropdown logic

--- src/components/section-switch/section-switch.tsx
+++ src/components/section-switch/section-switch.tsx
@@ -8,18 +8,42 @@ import { Ionic } from '../../icons';
 export class SectionSwitch {
   @Prop() section: string;
 
-  render() {
-    return [
-      <div class="section-switch__selector">
+  renderSelector(dropdown) {
+    return (
+      <div class={{ 'section-switch__selector': true, 'is-open': dropdown.isOpen }}>
         <a class="section-switch__logo" href="/docs">
           <Ionic/>
           <span>Docs</span>
         </a>
-        <span class="section-switch__current">
+        <span class="section-switch__selected" onClick={dropdown.toggle}>
           { this.section }
           <svg viewBox="0 0 33 22"><polygon points="16.5 22 0 0 33 0"></polygon></svg>
         </span>
       </div>
-    ];
+    );
+  }
+
+  renderPanel(dropdown) {
+    return (
+      <ul class={{ 'section-switch__panel': true, 'is-open': dropdown.isOpen }}>
+        <li>Framework</li>
+        <li>Pro</li>
+        <li>CLI</li>
+        <li>Stencil</li>
+        <li>Capacitor</li>
+      </ul>
+    );
+  }
+
+  render() {
+    return (
+      <ctrl-dropdown
+        class="section-switch"
+        autoClose
+        renderer={dropdown => [
+          this.renderSelector(dropdown),
+          this.renderPanel(dropdown)
+        ]}/>
+    );
   }
 }
