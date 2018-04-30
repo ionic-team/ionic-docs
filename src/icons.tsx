fb1930acca9545d9e9c4e994d0213d88b97bf487
Cam Wiegert <cam@camwiegert.com>
2018-04-30 12:53:06 -0500

    Add NewTab icon

--- src/icons.tsx
+++ src/icons.tsx
@@ -21,3 +21,9 @@ export const Ionic = () => (
   </svg>
 );
 
+export const NewTab = () => (
+  <svg class="new-tab" viewBox="0 0 43 42">
+    <rect class="new-tab__box" y="8" width="34" height="34" rx="6"/>
+    <path class="new-tab__arrow" d="M37.078 3.268H23.617V.243h18.626v18.625h-3.026V5.407L16.13 28.494l-2.14-2.139z"/>
+  </svg>
+);
