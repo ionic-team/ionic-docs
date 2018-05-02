d3f875cf0ff25dc2e7e8c6f5627ae0ddcc0d1a54
perry <perry@ionic.io>
2018-05-02 14:22:59 -0500

    windows being difficult with browser sync

--- bs-config.js
+++ bs-config.js
@@ -40,7 +40,7 @@ module.exports = {
     "logSnippet": true,
     "rewriteRules": [],
     "open": "local",
-    "browser": "google chrome",
+    "browser": "default",
     "cors": false,
     "xip": false,
     "hostnameSuffix": false,
