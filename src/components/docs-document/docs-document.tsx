3b25132a16b89a53d5638cf0b99fca907d442a91
perry <perry@ionic.io>
2018-05-17 12:54:41 -0500

    amend(TOC): accidental console log left in last commit

--- src/components/docs-document/docs-document.tsx
+++ src/components/docs-document/docs-document.tsx
@@ -63,7 +63,6 @@ export class DocsDocument {
     this.hideTOC = attributes.hideTOC;
     this.frontMatter = attributes;
     this.tocHeadings = this.hideTOC ? [] : headings;
-    console.log(headings);
     this.attributes = attributes;
     this.pageClass = this.path.replace(/\//g, '-');
   }
