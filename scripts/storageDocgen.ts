4008139177a68bea6505e245f021e52b08457987
perry <perry@ionic.io>
2018-05-31 12:00:54 -0500

    storage fix

--- scripts/storageDocgen.ts
+++ scripts/storageDocgen.ts
@@ -1,49 +1,49 @@
 export function getMarkup(data): string {
   return `
-  # Storage
+# Storage
 
-  <p class="intro">${data.comment.shortText}</p>
+<p class="intro">${data.comment.shortText}</p>
 
-  ${data.comment.text}
+${data.comment.text}
 
-  ## Usage
+## Usage
 
-  ${data.comment.tags[0].text}
+${data.comment.tags[0].text}
 
-  ## Instance Members
+## Instance Members
 
-  ${data.children.map(member => {
-    if (member.name.charAt(0) === '_') return;
+${data.children.map(member => {
+  if (member.name.charAt(0) === '_') return;
 
-    if (
-      member.kindString === 'Constructor' ||
-      member.kindString === 'Accessor'
-    ) return `
-  ### ${member.name}
+  if (
+    member.kindString === 'Constructor' ||
+    member.kindString === 'Accessor'
+  ) return `
+### ${member.name}
 
-  ${member.comment.shortText ? member.comment.shortText : ''}
+${member.comment.shortText ? member.comment.shortText : ''}
 
-  ${member.comment.text ? member.comment.text : ''}
-  <br><br>`;
+${member.comment.text ? member.comment.text : ''}
+<br><br>`;
 
-    return `
+  return `
 
-  ### ${member.name}${member.kindString === 'Method' ? '()' : ''}
-  ${member.signatures[0].comment.shortText ?
-    member.signatures[0].comment.shortText : ''}
-  ${member.signatures[0].comment.returns ? `
-  __Returns:__ ${member.signatures[0].comment.returns}
+### ${member.name}${member.kindString === 'Method' ? '()' : ''}
+${member.signatures[0].comment.shortText ?
+  member.signatures[0].comment.shortText : ''}
+${member.signatures[0].comment.returns ? `
+__Returns:__ ${member.signatures[0].comment.returns}
 
-  ` : ''}
+` : ''}
 
-  ${member.signatures[0].parameters ? `<h4>Parameters</h4>
-  <dl>` : ''}${
-    member.signatures[0].parameters ?
-    member.signatures[0].parameters.map(param => `<dt><h5>${param.name}</h5>${
-  param.type.type === 'intrinsic' ?
-    `<strong>Type:</strong> <code>${param.type.name}</code>` : ''}</dt><dd>${
-  param.comment.text}</dd>`).join('') : ''}${
-  member.signatures[0].parameters ? '</dl>' : ''}<br><br>`;
-  }).join('\n\n')}
-  `;
-  }
+${member.signatures[0].parameters ? `<h4>Parameters</h4>
+<dl>` : ''}${
+  member.signatures[0].parameters ?
+  member.signatures[0].parameters.map(param => `<dt><h5>${param.name}</h5>${
+param.type.type === 'intrinsic' ?
+  `<strong>Type:</strong> <code>${param.type.name}</code>` : ''}</dt><dd>${
+param.comment.text}</dd>`).join('') : ''}${
+member.signatures[0].parameters ? '</dl>' : ''}<br><br>`;
+}).join('\n\n')}
+`;
+}
