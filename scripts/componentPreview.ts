40f4326dcd699a4a3fd76c5b70996864d2c535a9
Cam Wiegert <cam@camwiegert.com>
2018-07-11 15:41:07 -0500

    docs(components): add intro

--- scripts/componentPreview.ts
+++ scripts/componentPreview.ts
@@ -15,6 +15,8 @@ previewSource: https://github.com/ionic-team/component-preview/tree/master/src/c
 hideTOC: true
 ---
 
+Ionic apps are made of high-level building blocks called components. Components allow you to quickly construct an interface for your app. Ionic comes with a number of components, including modals, popups, and cards. Check out the examples below to see what each component looks like and to learn how to use each one. Once you’re familiar with the basics, head over to the [API docs](/docs/api) for ideas on how to customize each component.
+
 <component-preview></component-preview>`;
 
 
