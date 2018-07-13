4d3b28ae7407afe7fc5bd1796960ffb060dc5b21
Cam Wiegert <cam@camwiegert.com>
2018-07-13 09:51:09 -0500

    fix(color-gen): upgrade stencil -> 0.10.4 and revert color-gen changes

--- src/components/color-gen/variable-selector/color-gen-variable-selector.tsx
+++ src/components/color-gen/variable-selector/color-gen-variable-selector.tsx
@@ -70,7 +70,7 @@ export class VariableSelector {
         'color-selector--error': this.showInputError
         }}>
         <div class="color-selector__name">
-          <i class="color-selector__swatch" style={{'background-color': this.value}}></i>
+          <i class="color-selector__swatch" style={{'backgroundColor': this.value}}></i>
           {this.name}
         </div>
         {(this.editable)
@@ -81,7 +81,7 @@ export class VariableSelector {
                 'color-selector__input--focused': this.isInputFocused
               }}
               onClick={ev => { if (this.isParentOpen) ev.stopPropagation(); }}>
-              <div class="color-selector__color-wrap" style={{'background-color': this.value}}>
+              <div class="color-selector__color-wrap" style={{'backgroundColor': this.value}}>
                 <input type="color"
                   value={this.value}
                   onInput={this.onColorChange.bind(this)}
