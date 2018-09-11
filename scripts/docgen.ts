a936f2fe565a4b8acb7ac30a46a79cf112318893
perry <perry@ionic.io>
2018-09-11 15:16:52 -0500

    refactoring docgen for prettier output in CI
    Also, less code is more

--- scripts/docgen.ts
+++ scripts/docgen.ts
@@ -3,7 +3,10 @@
 import { config as dotenv } from 'dotenv';
 dotenv({ silent: true });
 import { existsSync } from 'fs';
+import isCI from 'is-ci';
 import Listr from 'listr';
+import listrVerboseRenderer from 'listr-verbose-renderer';
+
 import { generate as apiDocs } from './api';
 import { generate as cliDocs } from './cli';
 import { generate as componentPreview } from './component-preview';
@@ -22,76 +25,28 @@ import {
 const program = require('commander');
 
 // the main task of the API documentation generation process
-const tasks = new Listr([
-  {
-    title: 'Precheck',
-    task: () => preCheck()
+const allTasks = {
+  'framework': {
+    title: 'API Docs',
+    task: (ctx, task) => tryToRun(apiDocs, task, 'API', API_DOCS_DIR)
   },
-  {
-    title: 'Generating',
-    task: () => {
-      return new Listr([
-        {
-          title: 'API Docs',
-          task: (ctx, task) => tryToRun(apiDocs, task, 'API', API_DOCS_DIR)
-        },
-        {
-          title: 'CLI Docs',
-          task: (ctx, task) => tryToRun(cliDocs, task, 'CLI', CLI_DOCS_DIR)
-        },
-        {
-          title: 'Component Preview',
-          task: (ctx, task) => tryToRun(componentPreview, task, 'Component Preview', COMPONENT_PREVIEW_DIR)
-        },
-        {
-          title: 'Native Docs',
-          task: (ctx, task) => tryToRun(nativeDocs, task, 'Native', NATIVE_DOCS_DIR)
-        },
-        {
-          title: 'Storage Docs',
-          task: (ctx, task) => tryToRun(storageDocs, task, 'Storage', STORAGE_DOCS_DIR)
-        }
-      ], { concurrent: true });
-    }
+  'cli': {
+    title: 'CLI Docs',
+    task: (ctx, task) => tryToRun(cliDocs, task, 'CLI', CLI_DOCS_DIR)
+  },
+  'component-preview': {
+    title: 'Component Preview',
+    task: (ctx, task) => tryToRun(componentPreview, task, 'Component Preview', COMPONENT_PREVIEW_DIR)
+  },
+  'native': {
+    title: 'Native Docs',
+    task: (ctx, task) => tryToRun(nativeDocs, task, 'Native', NATIVE_DOCS_DIR)
+  },
+  'storage': {
+    title: 'Storage Docs',
+    task: (ctx, task) => tryToRun(storageDocs, task, 'Storage', STORAGE_DOCS_DIR)
   }
-]);
-
-// for debugging purposes
-async function singleSectionRun(section) {
-  setVerbose(true);
-
-  vlog(`Starting individual docgen task: ${section}`);
-  if (!(await preCheck())) {
-    console.error('Docs Precheck Failure. Check configs and readme.');
-    return;
-  } else {
-    vlog('Precheck complete');
-  }
-
-  const taskPolyfill = {
-    set output(str) {
-      console.log(str);
-    }
-  };
-
-  switch (section) {
-    case 'cli':
-      await tryToRun(cliDocs, taskPolyfill, 'CLI', CLI_DOCS_DIR).catch();
-      break;
-    case 'components':
-      await tryToRun(componentPreview, taskPolyfill, 'Component', COMPONENT_PREVIEW_DIR).catch();
-      break;
-    case 'native':
-      await tryToRun(nativeDocs, taskPolyfill, 'Native', NATIVE_DOCS_DIR).catch();
-      break;
-    case 'storage':
-      await tryToRun(storageDocs, taskPolyfill, 'Storage', STORAGE_DOCS_DIR).catch();
-      break;
-    default:
-      // framework is default
-      await tryToRun(apiDocs, taskPolyfill, 'API', API_DOCS_DIR).catch();
-  }
-}
+};
 
 // Invoke run() only if executed directly i.e. `node ./scripts/e2e`
 if (require.main === module) {
@@ -99,17 +54,33 @@ if (require.main === module) {
     .version('0.1.0')
     .option(
       '-s, --section [name]',
+      '-v, --verbose',
       'Individual section to generate: framework, cli, component, native, storage'
     ).parse(process.argv);
 
-  if (program.section && program.section !== 'all') {
-    singleSectionRun(program.section);
-  } else {
-    tasks.run();
-  }
-}
+  const singleSectionRun = program.section && program.section !== 'all';
 
-function exit(err) {
-  console.log(err);
-  process.exit(1);
+  const listrOpts: any = {};
+  if (isCI || program.verbose || singleSectionRun) {
+    listrOpts.renderer = listrVerboseRenderer;
+    setVerbose(true);
+  }
+
+  const chosenTasks = singleSectionRun ?
+    [allTasks[program.section]] : Object.values(allTasks);
+
+  const queue = new Listr([
+    {
+      title: 'Precheck',
+      task: () => preCheck()
+    },
+    {
+      title: 'Generating',
+      task: () => {
+        return new Listr(chosenTasks, { concurrent: true });
+      }
+    }
+  ], listrOpts);
+
+  queue.run();
 }
