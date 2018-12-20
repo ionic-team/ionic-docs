b362c72de5f50342e81f96479d4c00e3f3b3badc
Cam Wiegert <cam@camwiegert.com>
2018-12-20 08:50:00 -0600

    feat(build/api): render usage sections markdown

--- scripts/build-documents/document-types/api.ts
+++ scripts/build-documents/document-types/api.ts
@@ -17,12 +17,18 @@ async function getAPIDocuments(): Promise<Document[]> {
   return components.map(component => {
     const title = component.tag.slice(4);
     const path = `${join(DOCUMENTS_DIR, 'api', title)}.json`;
-    const { readme, ...contents } = component;
+    const { readme, usage, ...contents } = component;
     return {
       title,
       path,
       body: markdownRenderer(readme),
+      usage: renderUsage(usage),
       ...contents
     };
   });
 }
+
+const renderUsage = (usage) => Object.keys(usage).reduce((out, key) => {
+  out[key] = markdownRenderer(usage[key]);
+  return out;
+}, {});
