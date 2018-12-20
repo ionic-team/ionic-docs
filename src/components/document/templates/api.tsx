26c20ff017553ec184e320ded6b2ce572ba37a39
Cam Wiegert <cam@camwiegert.com>
2018-12-20 10:03:22 -0600

    feat(api): render usage examples

--- src/components/document/templates/api.tsx
+++ src/components/document/templates/api.tsx
@@ -1,11 +1,38 @@
 export default (props) => {
-  const { document } = props;
+  const { document, history } = props;
+  const usage = renderUsage(document.usage);
+  const headings = [...document.headings];
+
+  if (usage) {
+    headings.push({
+      text: 'Usage',
+      href: '#usage'
+    });
+  }
 
   return (
     <main>
       <h1>{ document.title }</h1>
-      <docs-table-of-contents links={document.headings} basepath={props.history.location.pathname}/>
+      <docs-table-of-contents links={headings} basepath={history.location.pathname}/>
       <div class="markdown-content" innerHTML={document.body}/>
+      { usage }
     </main>
+  );
+};
+
+const renderUsage = (usage) => {
+  const keys = Object.keys(usage);
+
+  if (!keys.length) {
+    return null;
+  }
+
+  return (
+    <section>
+      <h2 id="usage">Usage</h2>
+      <docs-tabs tabs={keys.join(',')}>
+        { keys.map(key => <div slot={key} innerHTML={usage[key]}/>) }
+      </docs-tabs>
+    </section>
   );
 };
