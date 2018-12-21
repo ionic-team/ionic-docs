10fd94dac1ac87090f058d37d200a6def7690da8
Cam Wiegert <cam@camwiegert.com>
2018-12-21 08:22:23 -0600

    feat(api): render custom props table

--- src/components/document/templates/api.tsx
+++ src/components/document/templates/api.tsx
@@ -1,6 +1,7 @@
 export default (props) => {
   const { document, history } = props;
   const usage = renderUsage(document.usage);
+  const customProps = renderCustomProps(document.styles);
   const headings = [...document.headings];
 
   if (usage) {
@@ -10,12 +11,20 @@ export default (props) => {
     });
   }
 
+  if (customProps) {
+    headings.push({
+      text: 'CSS Custom Properties',
+      href: '#css-custom-properties'
+    });
+  }
+
   return (
     <main>
       <h1>{ document.title }</h1>
       <docs-table-of-contents links={headings} basepath={history.location.pathname}/>
       <div class="markdown-content" innerHTML={document.body}/>
       { usage }
+      { customProps }
     </main>
   );
 };
@@ -33,6 +42,34 @@ const renderUsage = (usage) => {
       <docs-tabs tabs={keys.join(',')}>
         { keys.map(key => <div slot={key} innerHTML={usage[key]}/>) }
       </docs-tabs>
+    </section>
+  );
+};
+
+const renderCustomProps = (customProps) => {
+  if (!customProps.length) {
+    return null;
+  }
+
+  return (
+    <section>
+      <h2 id="css-custom-properties">CSS Custom Properties</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Description</th>
+          </tr>
+        </thead>
+        <tbody>
+          {customProps.map(prop => (
+            <tr>
+              <td><code>{ prop.name }</code></td>
+              <td>{ prop.docs }</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </section>
   );
 };
