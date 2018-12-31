b893ef3b15991c406be24b9a3ea06038feab1100
Cam Wiegert <cam@camwiegert.com>
2018-12-31 14:02:22 -0600

    feat(utils): add keyBy util

--- src/utils.ts
+++ src/utils.ts
@@ -1,2 +1,12 @@
 export const slugify = (text: string) =>
   text.toLowerCase().replace(/[^\w]+/g, '-');
+
+export const keyBy = (
+  array: { [key: string]: any }[],
+  keyFunc: (item: any) => string,
+  valFunc?: (item: any) => any
+) =>
+  array.reduce((obj, item) => {
+    obj[keyFunc(item)] = valFunc ? valFunc(item) : item;
+    return obj;
+  }, {});
