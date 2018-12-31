5b7f18771b20e4d14cfeacdccacc628d5b113a02
Cam Wiegert <cam@camwiegert.com>
2018-12-31 14:02:51 -0600

    feat(scripts): add build-menu-data script

---
+++ scripts/build-menu-data/index.ts
@@ -0,0 +1,40 @@
+import { components } from '@ionic/docs/core.json';
+import { commands } from '../data/cli.json';
+import { join, resolve } from 'path';
+import { keyBy, slugify } from '../../src/utils';
+import Listr from 'listr';
+import fs from 'fs-extra';
+
+const MENU_DATA_DIR = resolve(__dirname, '../../src/components/menu/data');
+
+const componentMenu = keyBy(
+  components,
+  (item) => item.tag.slice(4),
+  (item) => `/docs/api/${slugify(item.tag.slice(4))}`
+);
+
+const cliCommandMenu = keyBy(
+  commands,
+  (item) => item.name.slice(6),
+  (item) => `/docs/cli/commands/${slugify(item.name.slice(6))}`
+);
+
+const tasks = new Listr([
+  {
+    title: 'Build API component menu data',
+    task: () => fs.outputJSON(
+      join(MENU_DATA_DIR, 'api-components.json'),
+      componentMenu,
+      { spaces: 2 }
+    )
+  },
+  {
+    title: 'Build CLI command menu data',
+    task: () => fs.outputJSON(
+      join(MENU_DATA_DIR, 'cli-commands.json'),
+      cliCommandMenu,
+      { spaces: 2 }
+    )
+  }
+]).run()
+  .catch(console.error);
