6118d8336620009d1caf8e180c762982ac7e082d
Cam Wiegert <cam@camwiegert.com>
2019-01-01 23:00:30 -0600

    feat(document): add more complete error template

--- src/components/document/templates/error.tsx
+++ src/components/document/templates/error.tsx
@@ -1,3 +1,34 @@
+import { sample } from '../../../utils';
+
 export default (response: Response) => (
-  <div>{ response.statusText }</div>
+  <main class="Error full-screen">
+    <div>{ sample(appropriateEmoji) }</div>
+    <h1>{ response.statusText }</h1>
+    <p>{ getHelpfulMessage(response) }</p>
+  </main>
 );
+
+const getHelpfulMessage = (response: Response) => {
+  switch (response.status) {
+    case 404:
+      return 'Sorry, we couldn\'t find that page.';
+    default:
+      return 'Sorry, something went wrong.';
+  }
+};
+
+const appropriateEmoji = [
+  '🧐',
+  '😕',
+  '😟',
+  '😑',
+  '😧',
+  '🤭',
+  '😯',
+  '😮',
+  '🤨',
+  '🙃',
+  '🤕',
+  '🤯',
+  '😫'
+];
