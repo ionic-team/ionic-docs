abd77ccd3d799ba9cbdd8695c21d20e38b3654af
Cam Wiegert <cam@camwiegert.com>
2019-01-09 16:06:31 -0600

    fix(document): create title from status code in error template
    Apparently, HTTP/2 no longer has status code text.

--- src/components/document/templates/error.tsx
+++ src/components/document/templates/error.tsx
@@ -3,10 +3,19 @@ import { sample } from '../../../utils';
 export default (response: Response) => (
   <main class="Error full-screen">
     <div>{ sample(appropriateEmoji) }</div>
-    <h1>{ response.statusText }</h1>
+    <h1>{ getHelpfulTitle(response) }</h1>
     <p>{ getHelpfulMessage(response) }</p>
   </main>
 );
+
+const getHelpfulTitle = (response: Response) => {
+  switch (response.status) {
+    case 404:
+      return 'Not Found';
+    default:
+      return 'Uh Oh';
+  }
+};
 
 const getHelpfulMessage = (response: Response) => {
   switch (response.status) {
