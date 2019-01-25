f44d860e7969cd6d8e3ace27ea1374faac96fce8
Brandy Carney <brandy@ionic.io>
2019-01-25 16:57:05 -0500

    feat(code-color): port code color component over

---
+++ src/components/code-color/code-color.tsx
@@ -0,0 +1,26 @@
+import { Component, Prop } from '@stencil/core';
+
+
+@Component({
+  tag: 'code-color',
+  styleUrl: 'code-color.css'
+})
+export class ColorBlock {
+  @Prop() value: string;
+
+  @Prop() display: string;
+
+  render() {
+    const display = this.display === undefined ? this.value.trim() : this.display.trim();
+
+    return [
+      <span
+        class="code-color-block"
+        style={{
+          'background-color': this.value
+        }}>
+      </span>,
+      <code class="code-color-value">{ display }</code>
+    ];
+  }
+}
