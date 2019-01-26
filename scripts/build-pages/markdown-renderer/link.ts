dc41c2ed89bf6a6f9cd702355d6dd9c942fdd92c
Lars Mikkelsen <lars@ionic.io>
2019-01-26 13:18:47 -0500

    fix: resolve markdown links to handle relative URLs

--- scripts/build-pages/markdown-renderer/link.ts
+++ scripts/build-pages/markdown-renderer/link.ts
@@ -1,9 +1,15 @@
+import { resolve } from 'url';
+
 const isInternal = /^\/docs\/.*/;
 
-export default (href: string, title: string, text: string) => {
+export default function(href: string, title: string, text: string) {
+  if (this.options.baseUrl && href.startsWith('../')) {
+    href = resolve(this.options.baseUrl, href);
+  }
+
   if (isInternal.test(href)) {
     return `<stencil-route-link url=${href} ${title ? `anchorTitle=${title}` : ''}>${text}</stencil-route-link>`;
   }
 
   return `<a href=${href} ${title ? `title=${title}` : ''}>${text}</a>`;
-};
+}
--- scripts/build-pages/page-types/api.ts
+++ scripts/build-pages/page-types/api.ts
@@ -17,25 +17,26 @@ async function getAPIPages(): Promise<Page[]> {
   return components.map(component => {
     const title = component.tag;
     const path = `${join(PAGES_DIR, 'api', title.slice(4))}.json`;
+    const baseUrl = `/docs/api/${title.slice(4)}/`;
     const { readme, usage, props, methods, ...contents } = component;
     return {
       title,
       path,
-      body: markdownRenderer(readme),
-      usage: renderUsage(usage),
-      props: renderDocsKey(props),
-      methods: renderDocsKey(methods),
+      body: markdownRenderer(readme, baseUrl),
+      usage: renderUsage(usage, baseUrl),
+      props: renderDocsKey(props, baseUrl),
+      methods: renderDocsKey(methods, baseUrl),
       ...contents
     };
   });
 }
 
-const renderUsage = (usage) => Object.keys(usage).reduce((out, key) => {
-  out[key] = markdownRenderer(usage[key]);
+const renderUsage = (usage, baseUrl) => Object.keys(usage).reduce((out, key) => {
+  out[key] = markdownRenderer(usage[key], baseUrl);
   return out;
 }, {});
 
-const renderDocsKey = (items) => items.map(item => ({
+const renderDocsKey = (items, baseUrl) => items.map(item => ({
   ...item,
-  docs: markdownRenderer(item.docs)
+  docs: markdownRenderer(item.docs, baseUrl)
 }));
