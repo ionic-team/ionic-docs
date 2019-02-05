6a176edf3da3d537806eaa1ecba4f8071eb0d380
Cam Wiegert <cam@camwiegert.com>
2019-02-05 16:21:35 -0600

    feat(page): use template key instead of switch to determine template

---
+++ src/components/page/templates/index.ts
@@ -0,0 +1,15 @@
+import defaultTemplate from './default';
+import apiIndex from './api-index';
+import api from './api';
+import cli from './cli';
+import error from './error';
+import native from './native';
+
+export default {
+  'default': defaultTemplate,
+  'api-index': apiIndex,
+  'api': api,
+  'cli': cli,
+  'error': error,
+  'native': native
+};
