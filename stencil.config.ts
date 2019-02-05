b23cc0547f8d26d3b98f07924e726f852b72db0b
Cam Wiegert <cam@camwiegert.com>
2019-02-04 20:29:14 -0600

    refactor: port demos to plain HTML

--- stencil.config.ts
+++ stencil.config.ts
@@ -14,6 +14,7 @@ export const config: Config = {
   ],
   copy: [
     { src: 'pages/**/*.json' },
+    { src: 'demos' },
     { src: 'components/color-gen/demo/index.html', dest: 'pages/theming/color-generator/index.html' },
     { src: '_redirects', dest: '../_redirects' },
     { src: 'robots.txt', dest: '../robots.txt' }
