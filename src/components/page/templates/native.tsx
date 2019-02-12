d3c58dd5fa4e26ec76df31d67f6d1616d6491236
Matt Netkow <matt.netkow@gmail.com>
2019-02-12 15:19:49 -0600

    added cordova issues card to plugin detail pages (#450)

--- src/components/page/templates/native.tsx
+++ src/components/page/templates/native.tsx
@@ -48,7 +48,19 @@ const renderRepo = (repo: string) => {
   }
 
   return (
-    <a href={repo} class="outbound" target="_blank"><GitBranch/> { repo }</a>
+    <section>
+      <a href={repo} class="outbound" target="_blank"><GitBranch/> { repo }</a>
+      
+      <h2>Stuck on a Cordova issue?</h2>
+      <docs-card class="cordova-ee-card" header="Don't waste precious time on plugin issues." href="https://ionicframework.com/sales?product_of_interest=Ionic%20Enterprise%20Engine" >
+        <img src="/docs/assets/icons/native-cordova-bot.png" class="cordova-ee-img" />
+        <p>If you're building a serious project, you can't afford to spend hours troubleshooting. Ionic's experts offer official 
+          maintenance, support, and integration help.
+        </p>
+        
+        <docs-button class="native-ee-detail" href="https://ionicframework.com/sales?product_of_interest=Ionic%20Enterprise%20Engine">Contact Us Today!</docs-button>
+      </docs-card>
+    </section>
   );
 };
 
