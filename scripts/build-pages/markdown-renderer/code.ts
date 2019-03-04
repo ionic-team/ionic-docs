53cc44e53e5d2b46c0762868940693bf8da5a783
Josh Thomas <josh@ionic.io>
2019-03-04 16:17:39 -0600

    updated from highlight to prismjs.

--- scripts/build-pages/markdown-renderer/code.ts
+++ scripts/build-pages/markdown-renderer/code.ts
@@ -1,8 +1,28 @@
-import hljs from 'highlight.js';
+import Prism from 'prismjs';
+import loadLanguages from 'prismjs/components/';
+
+const languages = ['tsx', 'bash', 'typescript', 'markup', 'css', 'json', 'javascript', 'html'];
+loadLanguages(languages);
 
 export default (code: string, info: string) => {
-  const [lang, ...params] = info.split(/\s+/);
+  let [lang] = info.split(/\s+/);
+
+  lang = lang.toLowerCase();
+
+  switch (lang) {
+  case 'shell':
+    lang = 'bash';
+    break;
+  case 'vue':
+    lang = 'html';
+    break;
+  }
+
+  if (lang != null && languages.indexOf(lang) !== -1) {
+    code = Prism.highlight(code, Prism.languages[lang]);
+  }
+
   return (
-    `<docs-code language="${lang}">${hljs.highlightAuto(code, [lang]).value}</docs-code>`
+    `<docs-code language="${lang}">${code}</docs-code>`
   );
 };
