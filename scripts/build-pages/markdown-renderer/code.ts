57f25ebbb0c3a251db5b49dd72e20b62bdae46d0
Josh Thomas <josh@ionic.io>
2019-03-05 14:44:00 -0600

    Update so that bash theme will only get cursor styled.

--- scripts/build-pages/markdown-renderer/code.ts
+++ scripts/build-pages/markdown-renderer/code.ts
@@ -1,15 +1,20 @@
 import Prism from 'prismjs';
 import loadLanguages from 'prismjs/components/';
 
-const languages = ['tsx', 'bash', 'typescript', 'markup', 'css', 'json', 'javascript', 'html'];
+const languages = ['tsx', 'typescript', 'markup', 'css', 'json', 'javascript', 'html'];
 loadLanguages(languages);
 
 export default (code: string, info: string) => {
   let [lang] = info.split(/\s+/);
+  lang = lang.toLowerCase();
 
-  lang = (lang === 'shell') ? 'bash' : lang.toLowerCase();
+  if (['shell', 'bash'].indexOf(lang) !== -1) {
+    lang = 'bash';
+    code = code.split(/\r?\n/)
+        .map(l => l.replace(/^\s*\$\s*/, '<span class="cursor"></span>'))
+        .join('\n');
 
-  if (lang != null && languages.indexOf(lang) !== -1) {
+  } else if (lang != null && languages.indexOf(lang) !== -1) {
     code = Prism.highlight(code, Prism.languages[lang]);
   }
 
