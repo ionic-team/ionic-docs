725a3408dd617e38aab444589dda87e5bf04a9b4
Cam Wiegert <cam@camwiegert.com>
2019-03-08 10:32:31 -0600

    fix(scripts): fail gracefully when unable to resolve the GitHub API (#539)

--- scripts/build-pages/page-types/static.ts
+++ scripts/build-pages/page-types/static.ts
@@ -51,31 +51,32 @@ const readMarkdown = (path: string): Promise<string> =>
 const getGitHubData = async (filePath: string) => {
   const [, path] = /^.+\/(src\/pages\/.+\.md)$/.exec(filePath);
   const since = new Date('2019-01-23').toISOString();
-  const request = await fetch(url.format({
-    protocol: 'https',
-    hostname: 'api.github.com',
-    pathname: 'repos/ionic-team/ionic-docs/commits',
-    query: {
-      access_token: process.env.GITHUB_API_TOKEN,
-      since,
-      path
-    }
-  }));
 
-  if (!request.ok) {
+  try {
+    const request = await fetch(url.format({
+      protocol: 'https',
+      hostname: 'api.github.com',
+      pathname: 'repos/ionic-team/ionic-docs/commits',
+      query: {
+        access_token: process.env.GITHUB_API_TOKEN,
+        since,
+        path
+      }
+    }));
+
+    const commits = await request.json();
+    const contributors = Array.from(new Set(commits.map(commit => commit.author.login)));
+    const lastUpdated = commits.length ? commits[0].commit.author.date : since;
+    return {
+      path,
+      contributors,
+      lastUpdated
+    };
+  } catch (error) {
     return {
       path,
       contributors: [],
       lastUpdated: since
     };
   }
-
-  const commits = await request.json();
-  const contributors = Array.from(new Set(commits.map(commit => commit.author.login)));
-  const lastUpdated = commits.length ? commits[0].commit.author.date : since;
-  return {
-    path,
-    contributors,
-    lastUpdated
-  };
 };
