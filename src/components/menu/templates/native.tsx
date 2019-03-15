820fb59361e7d5fc570818bf93eda817e109f541
Cam Wiegert <cam@camwiegert.com>
2019-03-15 13:46:37 -0500

    fix: add leading slash to new native menu links

--- src/components/menu/templates/native.tsx
+++ src/components/menu/templates/native.tsx
@@ -6,8 +6,8 @@ const items = {
   'Enterprise': {
     'Native Core': '/docs/native/native-core',
     'Active Directory': 'https://ionicframework.com/integrations/ms-activedirectory-ms-adal',
-    'Identity Vault': 'docs/native/identity-vault',
-    'mParticle': 'docs/native/mparticle',
+    'Identity Vault': '/docs/native/identity-vault',
+    'mParticle': '/docs/native/mparticle',
     'Offline Support': 'https://ionicframework.com/integrations/couchbase-lite'
   },
   'Plugins': Object.entries(plugins).sort()
