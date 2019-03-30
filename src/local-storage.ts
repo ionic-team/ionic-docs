7bb2df44142de77d739784de85d7bb3a15e3d92d
Cam Wiegert <cam@camwiegert.com>
2019-03-30 08:29:24 -0500

    feat: add local-storage module

---
+++ src/local-storage.ts
@@ -0,0 +1,29 @@
+function getItem(key: string): string {
+  try {
+    return localStorage.getItem(key);
+  } catch (error) {
+    console.warn(`Unable to get key "${key}" from localStorage:`, error);
+    return null;
+  }
+}
+
+function setItem(key: string, value: string): void {
+  try {
+    localStorage.setItem(key, value);
+  } catch (error) {
+    console.warn(`Unable to set key "${key}" of localStorage:`, error);
+  }
+}
+
+type LocalStorageGetter = () => string;
+type LocalStorageSetter = (value: string) => void;
+
+export function useLocalStorage(key: string): [
+  LocalStorageGetter,
+  LocalStorageSetter
+] {
+  return [
+    () => getItem(key),
+    (value: string) => setItem(key, value)
+  ];
+}
