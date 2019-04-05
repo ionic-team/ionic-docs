8f6ceb6a396103436a36f338aa541ac3b3f9c6d1
Cam Wiegert <cam@camwiegert.com>
2019-04-05 11:04:48 -0500

    feat(local-storage): emit 'local-storage' on update

--- src/local-storage.ts
+++ src/local-storage.ts
@@ -9,11 +9,23 @@ function getItem(key: string): string {
 
 function setItem(key: string, value: string): string {
   try {
+    if (localStorage.getItem(key) === value) {
+      return value;
+    }
+
     localStorage.setItem(key, value);
+    notify(key, value);
     return value;
   } catch (error) {
     console.warn(`Unable to set key "${key}" of localStorage:`, error);
     return null;
+  }
+}
+
+function notify(key: string, value: string) {
+  if (window && typeof window.dispatchEvent === 'function') {
+    const detail = { key, value };
+    window.dispatchEvent(new CustomEvent('local-storage', { detail }));
   }
 }
 
