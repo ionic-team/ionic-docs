7718b7f81264f115c6a72158219302516481011f
榊原昌彦 <sakakibara@rdlabo.jp>
2019-04-29 13:16:59 +0900

    翻訳システムをCLIとNativeに拡充

--- scripts/translate.ts
+++ scripts/translate.ts
@@ -1,16 +1,36 @@
-const documentDirectories = ['api', 'cli', 'native'];
+import { existsSync, mkdirSync, readFileSync, readdirSync, writeFileSync } from 'fs';
 import { components } from '@ionic/docs/core.json';
+import { commands } from './data/cli.json';
+
+const translateTypes = [
+  {
+    type: 'api',
+    contents: components,
+    key: 'tag',
+    md: 'readme',
+  },
+  {
+    type: 'cli',
+    contents: commands,
+    key: 'name',
+    md: 'description',
+  },
+  {
+    type: 'native',
+    contents: require('./data/native.json'),
+    key: 'packageName',
+    md: 'description',
+  }
+];
 
 async function apply() {
-  const fs = require('fs');
   let fileList: string[] | any = [];
-
-  for (const d of documentDirectories) {
-    const directory = process.cwd() + '/src/translate/' + d;
-    if (!fs.existsSync(directory)) {
+  for (const d of translateTypes) {
+    const directory = process.cwd() + '/src/translate/' + d.type;
+    if (!existsSync(directory)) {
       continue;
     }
-    let files: string[] | any = fs.readdirSync(directory, { encoding: 'UTF8' });
+    let files: string[] | any = readdirSync(directory, { encoding: 'UTF8' });
     files = files
       .filter(file => {
         return (/.*\.json$/.test(file) && !file.match(/readme/));
@@ -23,55 +43,77 @@ async function apply() {
 
   const componentsObject: object[] = [];
   for (const path of fileList) {
-    const componentObject = JSON.parse(fs.readFileSync(path, { encoding: 'UTF8' }));
+    const componentObject = JSON.parse(readFileSync(path, { encoding: 'UTF8' }));
     const readmePath = path.replace(/\.json/g, '.readme.md');
-    if (fs.existsSync(readmePath)) {
-      componentObject.readme = fs.readFileSync(readmePath, { encoding: 'UTF8' });
+    if (existsSync(readmePath)) {
+      componentObject.readme = readFileSync(readmePath, { encoding: 'UTF8' });
     }
     componentsObject.push(componentObject);
   }
   const corePath = process.cwd() + '/scripts/data/core.json';
-  const core = JSON.parse(fs.readFileSync(corePath, { encoding: 'UTF8' }));
+  const core = JSON.parse(readFileSync(corePath, { encoding: 'UTF8' }));
   core.components = componentsObject;
 
-  fs.writeFileSync(process.cwd() + '/scripts/data/core.json', JSON.stringify(core, null, 2), { encoding: 'UTF8' });
+  writeFileSync(process.cwd() + '/scripts/data/core.json', JSON.stringify(core, null, 2), { encoding: 'UTF8' });
 }
 
 async function create() {
-  const fs = require('fs');
-  components.map(component => {
-    const files = {
-      real: process.cwd() + '/src/translate/api/' + component.tag.replace('ion-', '') + '.json',
-      realReadme: process.cwd() + '/src/translate/api/' + component.tag.replace('ion-', '') + '.readme.md',
-      shadow: process.cwd() + '/src/translate/.api/' + component.tag.replace('ion-', '') + '.json',
-      shadowReadme: process.cwd() + '/src/translate/.api/' + component.tag.replace('ion-', '') + '.readme.md'
-    };
+  for (const translateType of translateTypes) {
+    translateType.contents.map(ob => {
+      const key = _changeNameToVariable(ob[translateType.key]);
+      const folder = {
+        real: process.cwd() + '/src/translate/' + translateType.type + '/',
+        shadow: process.cwd() + '/src/translate/.' + translateType.type + '/',
+      };
+      const files = {
+        real: folder.real + key + '.json',
+        realReadme: folder.real + key + '.readme.md',
+        shadow: folder.shadow + key + '.json',
+        shadowReadme: folder.shadow + key + '.readme.md'
+      };
 
-    if (!fs.existsSync(files.real)) {
-      fs.writeFileSync(files.real, JSON.stringify(component, null, 2), { encoding: 'UTF8' });
-    }
+      if (!existsSync(folder.real)) {
+        mkdirSync(folder.real);
+      }
 
-    if (!fs.existsSync(files.realReadme)) {
-      fs.writeFileSync(files.realReadme, component.readme, { encoding: 'UTF8' });
-    }
+      if (!existsSync(folder.shadow)) {
+        mkdirSync(folder.shadow);
+      }
 
-    fs.writeFileSync(files.shadow, JSON.stringify(component, null, 2), { encoding: 'UTF8' });
-    fs.writeFileSync(files.shadowReadme, component.readme, { encoding: 'UTF8' });
-  });
+      if (!existsSync(files.real)) {
+        writeFileSync(files.real, JSON.stringify(ob, null, 2), { encoding: 'UTF8' });
+      }
+
+      if (!existsSync(files.realReadme)) {
+        writeFileSync(files.realReadme, ob[translateType.md], { encoding: 'UTF8' });
+      }
+
+      writeFileSync(files.shadow, JSON.stringify(ob, null, 2), { encoding: 'UTF8' });
+      writeFileSync(files.shadowReadme, ob[translateType.md], { encoding: 'UTF8' });
+    });
+  }
 }
 
 async function diff() {
-  const fs = require('fs');
   const execSync = require('child_process').execSync;
   execSync('git diff src/translate/.api/*  > structure_api.patch');
 
   const patchPath = process.cwd() + '/structure_api.patch';
-  const patchTxt = fs.readFileSync(patchPath, { encoding: 'UTF8' });
+  const patchTxt = readFileSync(patchPath, { encoding: 'UTF8' });
   console.log(patchTxt.replace(/\.api/g, 'api'));
-  fs.writeFileSync(patchPath, patchTxt.replace(/\.api/g, 'api'), { encoding: 'UTF8' });
+  writeFileSync(patchPath, patchTxt.replace(/\.api/g, 'api'), { encoding: 'UTF8' });
 
   execSync('patch -p1 < \'structure_api.patch\'');
   execSync('rm structure_api.patch');
+}
+
+function _changeNameToVariable(name: string) {
+  return name
+    .replace('ion-', '')
+    .replace('@ionic-native/', '')
+    .replace('ionic ', '')
+    .replace(/ /g, '-')
+    .replace(/\//g, '-');
 }
 
 (async () => {
