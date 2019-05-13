b07c3acfd795da9a1c4d6306debf250385f68fef
perry <perry@ionic.io>
2019-05-13 14:49:23 -0500

    translations nav dropdown - active arrow

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -105,8 +105,13 @@ export class DocsHeader {
           </docs-dropdown>
           <docs-dropdown label="翻訳" align="right">
             <section>
-              <a href="https://ionicframework.com/docs/" target="_blank">English</a>
-              <a href="https://ionicframework.com/jp/docs/" target="_blank">日本語</a>
+              <a href="https://ionicframework.com/docs/">English</a>
+              <a href="https://ionicframework.com/jp/docs/" class="link-active">
+                日本語
+                <svg viewBox="0 0 512 512" width="14">
+                  <path d="M186.301 339.893L96 249.461l-32 30.507L186.301 402 448 140.506 416 110z"></path>
+                </svg>
+              </a>
             </section>
             <section>
               <a href="https://ionicframework.com/trasnlate" target="_blank">Translate</a>
