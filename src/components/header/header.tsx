3a1276c63ce748afcc1f68be546d39f34a620c79
perry <perry@ionic.io>
2019-05-13 13:33:41 -0500

    adding translation dropdown

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -103,6 +103,15 @@ export class DocsHeader {
               <a href="https://ionicframework.com/advisory" target="_blank">Enterprise Advisory</a>
             </section>
           </docs-dropdown>
+          <docs-dropdown label="Translations" align="right">
+            <section>
+              <a href="https://ionicframework.com/docs/" target="_blank">English</a>
+              <a href="https://ionicframework.com/jp/docs/" target="_blank">日本語</a>
+            </section>
+            <section>
+              <a href="https://ionicframework.com/trasnlate" target="_blank">Translate</a>
+            </section>
+          </docs-dropdown>
           <a class="outbound" href="https://github.com/ionic-team/ionic" target="_blank">GitHub <Outbound/></a>
         </nav>
       </header>
