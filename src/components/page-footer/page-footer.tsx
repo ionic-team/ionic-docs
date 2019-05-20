149f13d3cf781267dca224f789596384c230bc33
榊原昌彦 <sakakibara@rdlabo.jp>
2019-05-20 16:27:56 +0900

    trans(footer): API翻訳先リンクを変更

--- src/components/page-footer/page-footer.tsx
+++ src/components/page-footer/page-footer.tsx
@@ -28,14 +28,14 @@ export class DocsPageFooter {
       lastUpdated
     } = page.github;
 
-    const editHref = `https://github.com/ionic-team/ionic-docs/edit/master/${path}`;
-    const updatedHref = `https://github.com/ionic-team/ionic-docs/commits/master/${path}`;
+    const editHref = `https://github.com/ionic-jp/ionic-docs/edit/master/${path}`;
+    const updatedHref = `https://github.com/ionic-jp/ionic-docs/commits/master/${path}`;
     const updatedText = new Date(lastUpdated).toISOString().slice(0, 10);
     const contributorHref = (contributor) => `${updatedHref}?author=${contributor}`;
 
     return [
       contributors.length ? <contributor-list contributors={contributors} link={contributorHref}/> : null,
-      <docs-button round href={editHref}>Edit this page <ForwardArrow/></docs-button>,
+      <docs-button round href={editHref}>翻訳する<ForwardArrow/></docs-button>,
       <a class="last-updated" href={updatedHref}>Updated {updatedText}</a>
     ];
   }
