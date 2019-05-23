e5f47087dab387288e7a2c3f6c90add4fa98596f
Lars Mikkelsen <lars@ionic.io>
2019-05-23 11:32:13 -0400

    fix(header): English locale link

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -145,7 +145,7 @@ export class DocsHeader {
           </docs-dropdown>
           <docs-dropdown icon={Translation} align="right">
             <section>
-              <a href="https://ionicframework.com/docs/" class="link-active">
+              <a href="https://ionicframework.com/en/docs/" class="link-active">
                 English
                 <svg viewBox="0 0 512 512" width="14">
                   <path d="M186.301 339.893L96 249.461l-32 30.507L186.301 402 448 140.506 416 110z"></path>
