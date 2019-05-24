1667ab0d1ba593b54192577d0478e43ecbad728f
榊原昌彦 <sakakibara@rdlabo.jp>
2019-05-24 10:57:17 +0900

    design(announcement): 少し強調

--- src/components/footer-announcement/footer-announcement.tsx
+++ src/components/footer-announcement/footer-announcement.tsx
@@ -7,7 +7,7 @@ import { Component } from '@stencil/core';
 export class DocsFooterAnnouncement {
   render() {
     return (
-      <section class="docs-footer-announcement">2019/06/02(日) Ionic Meetup #11 Tokyo
+      <section class="docs-footer-announcement"><strong>Ionic Meetup #11 Tokyo:</strong> 2019/06/02(日)に開催
         <a href="https://ionic-jp.connpass.com/event/130785/" target="_blank">参加する</a>
       </section>
     );
