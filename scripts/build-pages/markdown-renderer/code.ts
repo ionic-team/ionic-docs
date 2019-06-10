3f7394e530736a6a6e535eb13571ecb966b29081
perry <perry@ionic.io>
2019-06-10 17:51:18 -0500

    prism js update 16 > 15

--- scripts/build-pages/markdown-renderer/code.ts
+++ scripts/build-pages/markdown-renderer/code.ts
@@ -11,7 +11,7 @@ export default (code: string, info: string) => {
   }
 
   return (
-    `<docs-code language="${language}">${Prism.highlight(code, Prism.languages[language])}</docs-code>`
+    `<docs-code language="${language}">${Prism.highlight(code, language ? Prism.languages[language] : null)}</docs-code>`
   );
 };
 
