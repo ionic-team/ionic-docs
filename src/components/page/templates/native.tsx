a86696454c8484871eab9d64a06138b67ac07d2f
perry <perry@ionic.io>
2019-06-17 15:45:38 -0500

    killing shadow cards

--- src/components/page/templates/native.tsx
+++ src/components/page/templates/native.tsx
@@ -52,16 +52,16 @@ const renderRepo = (repo: string) => {
   }
 
   return (
-    <section hidden>
+    <section>
       <a href={repo} class="outbound" target="_blank"><GitBranch/> { repo }</a>
       <h2>Stuck on a Cordova issue?</h2>
-      <docs-shadow-card class="cordova-ee-card" header="Don't waste precious time on plugin issues." href="https://ionicframework.com/sales?product_of_interest=Ionic%20Enterprise%20Engine">
+      <docs-card class="cordova-ee-card" header="Don't waste precious time on plugin issues." href="https://ionicframework.com/sales?product_of_interest=Ionic%20Enterprise%20Engine">
         <div>
           <img src="/docs/assets/icons/native-cordova-bot.png" class="cordova-ee-img" />
           <p>If you're building a serious project, you can't afford to spend hours troubleshooting. Ionic's experts offer official maintenance, support, and integration help.</p>
           <docs-button class="native-ee-detail" href="https://ionicframework.com/sales?product_of_interest=Ionic%20Enterprise%20Engine">Contact Us Today!</docs-button>
         </div>
-      </docs-shadow-card>
+      </docs-card>
     </section>
   );
 };
