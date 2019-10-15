bf54500dd0c9c46afc7a203cf77d4b701b56c98e
Matt Netkow <matt.netkow@gmail.com>
2019-10-15 12:54:12 -0500

    docs(native): Landing page tweaks (#1024)

--- src/components/menu/templates/native-landing.tsx
+++ src/components/menu/templates/native-landing.tsx
@@ -1,23 +1,32 @@
 import { h } from '@stencil/core';
-import plugins from '../data/native-plugins.json';
 
-export default () => <docs-nav items={items}/>;
-const communityPlugins = Object.entries(plugins).sort().slice(0, 6);
-communityPlugins.unshift(['menu-native-community-overview', '/docs/native/overview']);
-communityPlugins.push(['menu-native-community-show-all', '/docs/native/overview']);
+export default () => <docs-nav items={ items } />;
+
 const items = {
-  'menu-native-paid': {
-    'menu-native-paid-overview': '/docs/enterprise',
-    'Biometric Security': '/docs/enterprise/identity-vault',
-    'Offline Storage': '/docs/enterprise/offline-storage',
-    'Single Sign-on': '/docs/enterprise/auth-connect',
+  'menu-native-paid': '/docs/enterprise',
+  'menu-enterprise-authentication-security': {
+    'Auth Connect': '/docs/enterprise/auth-connect',
+    'Identity Vault': '/docs/enterprise/identity-vault',
+    'Offline Storage': '/docs/enterprise/offline-storage'
+  },
+  'menu-enterprise-common-device-features': {
+    'Apple Payment Pass': '/docs/enterprise/apple-payment-pass',
     'Browser': '/docs/enterprise/inappbrowser',
     'Camera': '/docs/enterprise/camera',
-    'Deeplinks': '/docs/enterprise/deeplinks',
+    'Contacts': '/docs/enterprise/contacts',
+    'Device': '/docs/enterprise/device',
     'Filesystem': '/docs/enterprise/filesystem',
     'Geolocation': '/docs/enterprise/geolocation',
+    'Media Capture': '/docs/enterprise/media-capture',
     'Network Information': '/docs/enterprise/network-information',
-    'menu-native-paid-show-all': '/docs/enterprise',
+    'Statusbar': '/docs/enterprise/statusbar',
+    'menu-native-paid-show-all': '/docs/enterprise'
   },
-  'menu-native-community': communityPlugins,
+  'menu-native-community': '/docs/native/overview',
+  'menu-native-community-oss': {
+    '3D Touch': '/docs/native/three-dee-touch',
+    'Backlight': '/docs/native/backlight',
+    'Health': '/docs/native/health',
+    'menu-native-community-show-all': '/docs/native/overview'
+  }
 };
