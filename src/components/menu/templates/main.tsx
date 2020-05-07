581b5b13f592d3d1f469de49f7e2630a014b10d6
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2020-05-07 13:34:14 -0400

    docs(hardware-back-button): add hardware back button documentation and examples (#1356)

--- src/components/menu/templates/main.tsx
+++ src/components/menu/templates/main.tsx
@@ -17,6 +17,7 @@ const items = {
     'menu-developing-ios': '/docs/developing/ios',
     'menu-developing-android': '/docs/developing/android',
     'menu-developing-tips': '/docs/developing/tips',
+    'menu-developing-hardware-back-button': '/docs/developing/hardware-back-button',
   },
   'menu-layout': {
     'menu-layout-structure': '/docs/layout/structure',
