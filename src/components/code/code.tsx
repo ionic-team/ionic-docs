acab11cd3c7f13e063f9762c29fda00a59d17124
jaredcbaum <jaredcbaum@gmail.com>
2020-05-13 17:06:33 -0500

    code button working

--- src/components/code/code.tsx
+++ src/components/code/code.tsx
@@ -1,4 +1,4 @@
-import { Component, Prop, State, Host, h, Element} from '@stencil/core';
+import { Component, Element, Host, Prop, State, h } from '@stencil/core';
 
 @Component({
   tag: 'docs-code',
@@ -35,16 +35,16 @@ export class DocsCode {
     return (
       <Host>
         { !['shell', 'bash', 'sh'].includes(this.language) ?
-        <div class={{ 'css-text__copy': true, 'show-confirmation': this.showCopyConfirmation }} >
-          <a class="css-text__copy-link" onClick={this.copyCodeText.bind(this)}>Copy</a>
-          <span class="css-text__copy-confirmation">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="26px" height="26px">
-              <path d="M362.6 192.9L345 174.8c-.7-.8-1.8-1.2-2.8-1.2-1.1 0-2.1.4-2.8 1.2l-122 122.9-44.4-44.4c-.8-.8-1.8-1.2-2.8-1.2-1 0-2 .4-2.8 1.2l-17.8 17.8c-1.6 1.6-1.6 4.1 0 5.7l56 56c3.6 3.6 8 5.7 11.7 5.7 5.3 0 9.9-3.9 11.6-5.5h.1l133.7-134.4c1.4-1.7 1.4-4.2-.1-5.7z"/>
-            </svg>
-            Copied
-          </span>
-        </div>
-        : ''}
+          <div class={{ 'css-text__copy': true, 'show-confirmation': this.showCopyConfirmation }} >
+            <a class="css-text__copy-link" onClick={this.copyCodeText.bind(this)}>Copy</a>
+            <span class="css-text__copy-confirmation">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="26px" height="26px">
+                <path d="M362.6 192.9L345 174.8c-.7-.8-1.8-1.2-2.8-1.2-1.1 0-2.1.4-2.8 1.2l-122 122.9-44.4-44.4c-.8-.8-1.8-1.2-2.8-1.2-1 0-2 .4-2.8 1.2l-17.8 17.8c-1.6 1.6-1.6 4.1 0 5.7l56 56c3.6 3.6 8 5.7 11.7 5.7 5.3 0 9.9-3.9 11.6-5.5h.1l133.7-134.4c1.4-1.7 1.4-4.2-.1-5.7z"/>
+              </svg>
+              Copied
+            </span>
+          </div>
+        : '' }
       </Host>
     );
   }
