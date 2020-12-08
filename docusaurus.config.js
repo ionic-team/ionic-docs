9fd73a2bc7a776ac9c34f3ddd5a32a63025dd5c8
Lars Mikkelsen <lars@ionic.io>
2020-12-07 21:21:40 -0500

    Update Docusaurus config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,100 +1,174 @@
 module.exports = {
-  title: 'My Site',
-  tagline: 'The tagline of my site',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  title: 'Ionic Documentation',
+  tagline: 'Ionic is the app platform for web developers. Build amazing mobile, web, and desktop apps all with one shared code base and open web standards',
+  url: 'https://ionicframework.com',
+  baseUrl: '/docs/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  favicon: 'img/favicon.png',
+  organizationName: 'ionic-team',
+  projectName: 'ionic-docs',
   themeConfig: {
     navbar: {
-      title: 'My Site',
       logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
+        alt: 'Ionic Logo',
+        src: 'img/framework-icon.svg',
       },
       items: [
-        {
-          to: 'docs/',
-          activeBasePath: 'docs',
-          label: 'Docs',
+         {
+          to: '/',
+          activeBaseRegex: '^/docs/(?!api|components)',
+          label: 'Guide',
           position: 'left',
         },
-        {to: 'blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
-          position: 'right',
+          to: '/components',
+          activeBaseRegex: '^/docs/(api|components)',
+          label: 'Components',
+          position: 'left',
         },
+        {
+          label: 'Platform',
+          position: 'right',
+          items: [
+            {
+              href: 'https://capacitorjs.com/docs',
+              label: 'Capacitor',
+              className: 'link--outbound',
+            },
+            {
+              href: 'https://ionicframework.com/docs',
+              label: 'Framework',
+              className: 'link--outbound',
+            },
+            {
+              to: '/appflow',
+              label: 'Appflow',
+            },
+            {
+              to: '/identity-vault',
+              label: 'Identity Vault',
+            },
+            {
+              to: '/auth-connect',
+              label: 'Auth Connect',
+            },
+            {
+              to: '/offline-storage',
+              label: 'Offline Storage',
+            },
+            {
+              to: '/premier-plugins',
+              label: 'Premier Plugins',
+            },
+          ],
+        },
+        // {
+        //   href: 'https://ionicframework.com/docs/cli',
+        //   label: 'CLI',
+        //   className: 'navbar-cli',
+        //   position: 'right',
+        // },
       ],
     },
-    footer: {
-      style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Style Guide',
-              to: 'docs/',
-            },
-            {
-              label: 'Second Doc',
-              to: 'docs/doc2/',
-            },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'Blog',
-              to: 'blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
-            },
-          ],
-        },
+    gtag: {
+      trackingID: 'UA-44023830-8',
+    },
+    prism: {
+      theme: { plain: {}, styles: [] },
+      // https://github.com/FormidableLabs/prism-react-renderer/blob/master/src/vendor/prism/includeLangs.js
+      additionalLanguages: [
+        'shell-session',
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
     },
   },
+  plugins: [
+    [
+      '@docusaurus/plugin-client-redirects',
+      {
+        redirects: [
+          { from: '/enterprise/auth-connect', to: '/auth-connect' },
+          {
+            from: '/enterprise/auth-connect/2.2.X/auth-connect',
+            to: '/auth-connect/2.2.X/auth-connect',
+          },
+          {
+            from: '/enterprise/auth-connect/3.0.X/auth-connect',
+            to: '/auth-connect/3.0.X/auth-connect',
+          },
+          { from: '/enterprise/auth-connect/auth0', to: '/auth-connect/auth0' },
+          {
+            from: '/enterprise/auth-connect/aws-cognito',
+            to: '/auth-connect/aws-cognito',
+          },
+          {
+            from: '/enterprise/auth-connect/azure-ad-b2c',
+            to: '/auth-connect/azure-ad-b2c',
+          },
+          {
+            from: '/enterprise/auth-connect/identityserver4',
+            to: '/auth-connect/identityserver4',
+          },
+          { from: '/enterprise/auth-connect/okta', to: '/auth-connect/okta' },
+          { from: '/enterprise/identity-vault', to: '/identity-vault' },
+          {
+            from: '/enterprise/identity-vault/2.0.X/identity-vault',
+            to: '/identity-vault/2.0.X/identity-vault',
+          },
+          {
+            from: '/enterprise/identity-vault/3.0.X/identity-vault',
+            to: '/identity-vault/3.0.X/identity-vault',
+          },
+          {
+            from: '/enterprise/identity-vault/3.1.X/identity-vault',
+            to: '/identity-vault/3.1.X/identity-vault',
+          },
+          {
+            from: '/enterprise/identity-vault/3.2.X/identity-vault',
+            to: '/identity-vault/3.2.X/identity-vault',
+          },
+          {
+            from: '/enterprise/identity-vault/3.3.X/identity-vault',
+            to: '/identity-vault/3.3.X/identity-vault',
+          },
+          {
+            from: '/enterprise/identity-vault/3.4.X/identity-vault',
+            to: '/identity-vault/3.4.X/identity-vault',
+          },
+          {
+            from: '/enterprise/identity-vault/3.5.X/identity-vault',
+            to: '/identity-vault/3.5.X/identity-vault',
+          },
+          {
+            from: '/enterprise/identity-vault/3.6.X/identity-vault',
+            to: '/identity-vault/3.6.X/identity-vault',
+          },
+          {
+            from: '/enterprise/identity-vault/4.0.X/identity-vault',
+            to: '/identity-vault/4.0.X/identity-vault',
+          },
+          {
+            from: '/enterprise/identity-vault/4.1.X/identity-vault',
+            to: '/identity-vault/4.1.X/identity-vault',
+          },
+          { from: '/enterprise/offline-storage', to: '/offline-storage' },
+          {
+            from: '/enterprise/offline-storage/1.0.X/offline-storage',
+            to: '/offline-storage/1.0.X/offline-storage',
+          },
+        ],
+      },
+    ],
+  ],
   presets: [
     [
       '@docusaurus/preset-classic',
       {
         docs: {
+          routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+          editUrl: 'https://github.com/ionic-team/ionic-docs/edit/main/',
+
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
