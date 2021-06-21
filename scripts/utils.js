f5c52acc216a1c460d21f95cbe18460aea76dfa5
Jared <jaredcbaum@gmail.com>
2021-06-21 13:01:55 -0500

    Build(release notes): Generating json file

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,6 +1,56 @@
-var unified = require('unified')
-var markdown = require('remark-parse')
-var html = require('remark-html')
+const unified = require('unified')
+const markdown = require('remark-parse')
+const html = require('remark-html')
+const { createDocument } = require('@stencil/core/mock-doc');
+
+var tagBlacklist = ['script', 'link', 'meta', 'object', 'head', 'html', 'body'];
+
+function convertHtmlToHypertextData(html) {
+  const doc = createDocument();
+  const div = doc.createElement('div');
+  div.innerHTML = html;
+  
+  return convertElementToHypertextData(div);
+};
+
+function convertElementToHypertextData(node) {
+  const data = [];
+
+  if (node.nodeType === 1) {
+    let tag = node.tagName.toLowerCase();
+
+    if (tagBlacklist.includes(tag)) {
+      tag = 'template';
+    }
+
+    data.push(tag);
+
+    if (node.attributes.length > 0) {
+      const attrs = {};
+      for (let j = 0; j < node.attributes.length; j++) {
+        const attr = node.attributes.item(j);
+        attrs[attr.nodeName] = attr.nodeValue;
+      }
+      data.push(attrs);
+
+    } else {
+      data.push(null);
+    }
+
+    for (const child of node.childNodes) {
+      data.push(convertElementToHypertextData(child));
+    }
+
+    console.log(data);
+
+    return data;
+
+  } else if (node.nodeType === 3) {
+    return node.textContent;
+  }
+
+  return '';
+};
 
 function renderMarkdown(markdownString) {
   return unified()
@@ -53,6 +103,7 @@ function renderOptionSpec(option){ return`
 function gitBranchSVG(){ return`<svg viewBox="0 0 512 512"><path d="M416 160c0-35.3-28.7-64-64-64s-64 28.7-64 64c0 23.7 12.9 44.3 32 55.4v8.6c0 19.9-7.8 33.7-25.3 44.9-15.4 9.8-38.1 17.1-67.5 21.5-14 2.1-25.7 6-35.2 10.7V151.4c19.1-11.1 32-31.7 32-55.4 0-35.3-28.7-64-64-64S96 60.7 96 96c0 23.7 12.9 44.3 32 55.4v209.2c-19.1 11.1-32 31.7-32 55.4 0 35.3 28.7 64 64 64s64-28.7 64-64c0-16.6-6.3-31.7-16.7-43.1 1.9-4.9 9.7-16.3 29.4-19.3 38.8-5.8 68.9-15.9 92.3-30.8 36-22.8 55-57 55-98.8v-8.6c19.1-11.1 32-31.7 32-55.4zM160 56c22.1 0 40 17.9 40 40s-17.9 40-40 40-40-17.9-40-40 17.9-40 40-40zm0 400c-22.1 0-40-17.9-40-40s17.9-40 40-40 40 17.9 40 40-17.9 40-40 40zm192-256c-22.1 0-40-17.9-40-40s17.9-40 40-40 40 17.9 40 40-17.9 40-40 40z"></path></svg>`;}
 
 module.exports = {
+  convertHtmlToHypertextData,
   gitBranchSVG,
   renderOptionSpec,
   renderMarkdown,
