4638b99a1560b1762c2222af5016fe47abdd1542
Jared <jaredcbaum@gmail.com>
2021-07-12 10:04:41 -0500

    fix(uselocalstorage): Check environment

--- src/theme/hooks/useLocalStorage.ts
+++ src/theme/hooks/useLocalStorage.ts
@@ -1,7 +1,8 @@
 import { useState } from 'react';
+import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
 
 export default function useLocalStorage<T>(key: string, initialValue: T) {
-  if (!window) return [null, null];
+  if (!ExecutionEnvironment.canUseDOM) return [null, null];
   // State to store our value
   // Pass initial state function to useState so logic is only executed once
   const [storedValue, setStoredValue] = useState<T>(() => {
