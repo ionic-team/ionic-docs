cb8abde5bc9f0cc1d7507c3d9e32d220816c7a41
jaredcbaum <jaredcbaum@gmail.com>
2021-07-26 12:12:24 -0500

    fix(): Remove Inter addition

--- docusaurus-theme/src/index.js
+++ docusaurus-theme/src/index.js
@@ -11,27 +11,5 @@ module.exports = function () {
     getClientModules() {
       return path.resolve(__dirname, './css/custom.scss');
     },
-
-    injectHtmlTags() {
-      return {
-        headTags: [
-          {
-            tagName: 'link',
-            attributes: {
-              rel: 'preconnect',
-              href: 'https://fonts.gstatic.com',
-            },
-          },
-          {
-            tagName: 'link',
-            attributes: {
-              rel: 'stylesheet',
-              href:
-                'https://fonts.googleapis.com/css2?family=Inter:wght@100..900&amp;display=swap',
-            },
-          },
-        ],
-      };
-    },
   };
 };
