200474100ef3b0b235492fd425b7e94158be4a8f
jaredcbaum <jaredcbaum@gmail.com>
2021-11-12 09:11:05 -0600

    feat(): Added docs demo, centered content

---
+++ src/theme/TOC/index.tsx
@@ -0,0 +1,14 @@
+import OriginalTOC from '@theme-original/TOC';
+import EditThisPage from '@theme/EditThisPage';
+import React from 'react';
+
+
+export default function TOC(props) {
+  return (
+    <div className="tocWrapper">
+      <h2>Contents</h2>
+      <OriginalTOC {...props} />
+      <EditThisPage />
+    </div>
+  );
+}
