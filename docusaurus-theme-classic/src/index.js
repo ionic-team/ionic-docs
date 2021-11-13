841b9954f106b17b99551a29858d1aec59aa064a
jaredcbaum <jaredcbaum@gmail.com>
2021-11-12 18:17:00 -0600

    fix(): Get search bar to show up

--- docusaurus-theme-classic/src/index.js
+++ docusaurus-theme-classic/src/index.js
@@ -1,11 +1,11 @@
 const path = require('path');
 const { Joi } = require('@docusaurus/utils-validation');
 
-const theme = require(path.resolve(__dirname, '../../node_modules/@docusaurus/theme-classic/lib'));
-const themePath = path.resolve(__dirname, '../../node_modules/@docusaurus/theme-classic/lib-next/theme');
-const tsThemePath = path.resolve(__dirname, '../../node_modules/@docusaurus/theme-classic/src/theme');
+const theme = require(path.resolve(__dirname, '../node_modules/@docusaurus/theme-classic/lib'));
+const themePath = path.resolve(__dirname, '../node_modules/@docusaurus/theme-classic/lib-next/theme');
+const tsThemePath = path.resolve(__dirname, '../node_modules/@docusaurus/theme-classic/src/theme');
 
-let { ThemeConfigSchema } = require(path.resolve(__dirname, '../../node_modules/@docusaurus/theme-classic/lib/validateThemeConfig.js'));
+let { ThemeConfigSchema } = require(path.resolve(__dirname, '../node_modules/@docusaurus/theme-classic/lib/validateThemeConfig.js'));
 
 const NavbarIconLinkSchema = Joi.object({
   type: Joi.string().equal('iconLink').required(),
