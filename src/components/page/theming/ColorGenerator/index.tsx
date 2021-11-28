40874f581e685190ede4dab048b340a3111bd31b
jaredcbaum <jaredcbaum@gmail.com>
2021-11-27 21:51:43 -0600

    chore(): Finished ColorGenerator component

--- src/components/page/theming/ColorGenerator/index.tsx
+++ src/components/page/theming/ColorGenerator/index.tsx
@@ -1,164 +1,152 @@
 import React, {
-  createContext,
-  useCallback,
-  useContext,
   useEffect,
   useRef,
   useState,
 } from 'react';
 
-import styles from './styles.module.css';
-import SelectColors from './SelectColors';
-
-import { MDXTag } from '@mdx-js/tag';
-// import { useMDXComponents } from '@mdx-js/react';
+import styles from './styles.module.scss';
 
 import {
-  convertCssToColors,
   generateColor,
-  updateCssText,
 } from '../_utils/index';
-import LiveCodeBlock from './LiveCodeBlock';
 
-interface ColorGeneratorProps {
-  mode?: 'ios' | 'md';
-  [key: string]: any;
-}
+import clsx from 'clsx';
 
-export const ColorGeneratorContext = createContext(undefined);
+import ColorDot from '../ColorDot';
+import ColorInput from '../ColorInput';
 
-const ATTR_MAP = {
-  value: '',
-  valueRgb: '-rgb',
-  contrast: '-contrast',
-  contrastRgb: '-contrast-rgb',
-  shade: '-shade',
-  tint: '-tint',
-};
+const ColorGenerator = (props) => {
+  const [colors, setColors] = useState({
+    primary: generateColor('#3880ff'),
+    secondary: generateColor('#5260ff'),
+    tertiary: generateColor('#5260ff'),
+    success: generateColor('#2dd36f'),
+    warning: generateColor('#ffc409'),
+    danger: generateColor('#eb445a'),
+    medium: generateColor('#92949c'),
+    light: generateColor('#f4f5f8'),
+  })
 
-const ColorGenerator = (props: ColorGeneratorProps) => {
-  const [cssText, setCssText] = useState(CSS_TEXT);
-  const [colors, setColors] = useState(convertCssToColors(CSS_TEXT));
+  const [activeColor, setActiveColor] = useState(null);
 
-  const textContent = useRef(CSS_TEXT);
+  const [cssText, setCssText] = useState('');
+
+  const codeRef = useRef<HTMLElement>(null);
 
   useEffect(() => {
-    setColors(convertCssToColors(cssText));
-    textContent.current = cssText;
-
     const event = new CustomEvent('demoMessage', { detail: { cssText } });
     window.dispatchEvent(event);
   }, [cssText]);
 
   useEffect(() => {
-    Array.from(colors).forEach(([_, color]) => {
-      const keys = (Object.keys(ATTR_MAP) as any) as (keyof typeof ATTR_MAP)[];
-      const genColor = generateColor(color.value);
-
-      for (const key of keys) {
-        setCssText(cssText =>
-          updateCssText(color.property + ATTR_MAP[key], cssText, genColor[key]),
-        );
-      }
-    });
-  }, [colors]);
+    setCssText(codeRef.current.textContent);
+  }, [colors])
 
   return (
-    <section className={styles.colorGenerator}>
-      <ColorGeneratorContext.Provider
-        value={{ controls: { setColors }, state: { colors } }}
-      >
-        <SelectColors />
-        {cssText && (
-          <MDXTag
-            name="code"
-            live
-            className="language-css"
-            components={{
-              code: props => (
-                <LiveCodeBlock
-                  {...props}
-                  language="css"
-                  onChange={code => (textContent.current = code)}
-                  onBlur={() => setCssText(textContent.current)}
-                  onMouseOut={() => setCssText(textContent.current)}
-                />
-              ),
-            }}
+    <section {...props} className={clsx(props.className, styles.colorGenerator)}>
+      <ul className={styles.selectColors}>
+      {Object.entries(colors).map(([name, color]) => {
+        const { tint, shade, value } = color;
+        const nameCap = name[0].toUpperCase() + name.substring(1);
+
+        const isOpen = activeColor === name ? true : false;
+
+        return (
+          <li
+            className={clsx(styles.item, { [styles.isOpen]: isOpen })}
+            onClick={() => setActiveColor(activeColor === name ? null : name)}
           >
-            {cssText}
-          </MDXTag>
-        )}
-      </ColorGeneratorContext.Provider>
+            <div className={styles.titleRow}>
+              <div className={styles.titleRowStart}>
+                <ColorDot color={value}/>
+                {nameCap}
+              </div>
+              <div  className={styles.titleRowEnd}>
+                <ColorInput
+                  onClick={ev => ev.stopPropagation()}
+                  color={value}
+                  setColor={(color) => setColors(colors => {
+                    colors[name] = generateColor(color);
+                    return {...colors};
+                  })}
+                />
+                <Caret />
+              </div>
+            </div>
+
+            <ul className={styles.submenu}>
+              <li className={styles.subcategory}>
+                <div className={styles.headingGroup}>
+                  <ColorDot color={shade} />
+                  <span>{nameCap}-shade</span>
+                </div>
+                <code>{shade}</code>
+              </li>
+              <li className={styles.subcategory}>
+                <div className={styles.headingGroup}>
+                  <ColorDot color={tint} />
+                  <span>{nameCap}-tint</span>
+                </div>
+                <code>{tint}</code>
+              </li>
+            </ul>
+          </li>
+        );
+      })}
+    </ul>
+    <pre className={styles.codePre}>
+      <code ref={codeRef}>
+        :root {'{'}{'\n'}
+        {Object.entries(colors).map(([name, color], i) => (
+          <>
+            {'\t'}--ion-color-{name}: {color.value};{'\n'}
+            {'\t'}--ion-color-{name}-rgb: {color.valueRgb};{'\n'}
+            {'\t'}--ion-color-{name}-contrast: {color.contrast};{'\n'}
+            {'\t'}--ion-color-{name}-contrast-rgb: {color.contrastRgb};{'\n'}
+            {'\t'}--ion-color-{name}-shade: {color.shade};{'\n'}
+            {'\t'}--ion-color-{name}-tint: {color.tint};{'\n'}
+            {'\n'}
+          </>
+        ))}
+        {'}'}{'\n'}
+      </code>
+    </pre>
     </section>
   );
 };
 
+const Caret = (props) => (
+  <svg
+    width="10px"
+    height="6px"
+    viewBox="0 0 10 6"
+    version="1.1"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <g
+      id="Welcome"
+      stroke="none"
+      strokeWidth="1"
+      fill="none"
+      fillRule="evenodd"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <g
+        id="Desktop-HD"
+        transform="translate(-1025.000000, -335.000000)"
+        stroke="#AEB4BE"
+        strokeWidth="2"
+      >
+        <polyline
+          id="arrow"
+          transform="translate(1030.000000, 338.000000) rotate(90.000000) translate(-1030.000000, -338.000000) "
+          points="1028 334 1032 338.020022 1028 342"
+        ></polyline>
+      </g>
+    </g>
+  </svg>
+);
+
 export default ColorGenerator;
-
-const CSS_TEXT = `
-:root {
-  --ion-color-primary: #3880ff;
-  --ion-color-primary-rgb: 56,128,255;
-  --ion-color-primary-contrast: #ffffff;
-  --ion-color-primary-contrast-rgb: 255,255,255;
-  --ion-color-primary-shade: #3171e0;
-  --ion-color-primary-tint: #4c8dff;
-
-  --ion-color-secondary: #3dc2ff;
-  --ion-color-secondary-rgb: 61,194,255;
-  --ion-color-secondary-contrast: #ffffff;
-  --ion-color-secondary-contrast-rgb: 255,255,255;
-  --ion-color-secondary-shade: #36abe0;
-  --ion-color-secondary-tint: #50c8ff;
-
-  --ion-color-tertiary: #5260ff;
-  --ion-color-tertiary-rgb: 82,96,255;
-  --ion-color-tertiary-contrast: #ffffff;
-  --ion-color-tertiary-contrast-rgb: 255,255,255;
-  --ion-color-tertiary-shade: #4854e0;
-  --ion-color-tertiary-tint: #6370ff;
-
-  --ion-color-success: #2dd36f;
-  --ion-color-success-rgb: 45,211,111;
-  --ion-color-success-contrast: #ffffff;
-  --ion-color-success-contrast-rgb: 255,255,255;
-  --ion-color-success-shade: #28ba62;
-  --ion-color-success-tint: #42d77d;
-
-  --ion-color-warning: #ffc409;
-  --ion-color-warning-rgb: 255,196,9;
-  --ion-color-warning-contrast: #000000;
-  --ion-color-warning-contrast-rgb: 0,0,0;
-  --ion-color-warning-shade: #e0ac08;
-  --ion-color-warning-tint: #ffca22;
-
-  --ion-color-danger: #eb445a;
-  --ion-color-danger-rgb: 235,68,90;
-  --ion-color-danger-contrast: #ffffff;
-  --ion-color-danger-contrast-rgb: 255,255,255;
-  --ion-color-danger-shade: #cf3c4f;
-  --ion-color-danger-tint: #ed576b;
-
-  --ion-color-dark: #222428;
-  --ion-color-dark-rgb: 34,36,40;
-  --ion-color-dark-contrast: #ffffff;
-  --ion-color-dark-contrast-rgb: 255,255,255;
-  --ion-color-dark-shade: #1e2023;
-  --ion-color-dark-tint: #383a3e;
-
-  --ion-color-medium: #92949c;
-  --ion-color-medium-rgb: 146,148,156;
-  --ion-color-medium-contrast: #ffffff;
-  --ion-color-medium-contrast-rgb: 255,255,255;
-  --ion-color-medium-shade: #808289;
-  --ion-color-medium-tint: #9d9fa6;
-
-  --ion-color-light: #f4f5f8;
-  --ion-color-light-rgb: 244,245,248;
-  --ion-color-light-contrast: #000000;
-  --ion-color-light-contrast-rgb: 0,0,0;
-  --ion-color-light-shade: #d7d8da;
-  --ion-color-light-tint: #f5f6f9;
-}
-`;
