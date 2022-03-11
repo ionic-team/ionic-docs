0b390c374960d4667b440cc599f8904c4c93d91e
Sean Perkins <sean@ionic.io>
2022-03-11 14:46:40 -0500

    fix(): correct path to package
    Co-authored-by: Liam DeBeasi <liamdebeasi@users.noreply.github.com>

--- scripts/api.js
+++ scripts/api.js
@@ -8,7 +8,7 @@ const DEMOS_PATH = path.resolve('static/demos');
 let COMPONENT_LINK_REGEXP;
 
 (async function () {
-  const response = await fetch('https://unpkg.com/@ionic/doc/core.json');
+  const response = await fetch('https://unpkg.com/@ionic/docs/core.json');
   const { components } = await response.json();
 
   const names = components.map((component) => component.tag.slice(4));
