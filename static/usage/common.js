9193b08019790c9577efa76ed9da11263a9aeb90
Liam DeBeasi <liamdebeasi@icloud.com>
2022-03-16 15:02:38 +0000

    chore(): add comments

--- static/usage/common.js
+++ static/usage/common.js
@@ -1,3 +1,11 @@
+/**
+ * The parent Playground component will
+ * dispatch messages whenever the theme in
+ * the Ionic Docs website has changed so that
+ * the demos loaded in iframes can match the
+ * docs website theme.
+ */
+
 window.addEventListener('message', (ev) => {
   const { data } = ev;
 
