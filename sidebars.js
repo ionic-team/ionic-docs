72a40ce69209cb1ca64d0c9c4446e6c29b752a0b
Anthony Giuliano <anthonymgiuliano@gmail.com>
2022-04-05 11:26:51 -0400

    adding vs code extension page

--- sidebars.js
+++ sidebars.js
@@ -4,7 +4,7 @@ module.exports = {
       type: 'category',
       label: 'Getting Started',
       collapsed: false,
-      items: ['index', 'intro/upgrading-to-ionic-6', 'intro/environment', 'intro/cli', 'intro/cdn', 'intro/next'],
+      items: ['index', 'intro/upgrading-to-ionic-6', 'intro/environment', 'intro/cli', 'intro/cdn', 'intro/vscode-extension', 'intro/next'],
     },
     {
       type: 'category',
