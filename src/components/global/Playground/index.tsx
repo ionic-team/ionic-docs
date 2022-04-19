985a7ccab4f44dc820f741ed8800166f43440450
Sean Perkins <sean@ionic.io>
2022-04-19 15:28:51 -0400

    chore(): vue icon

--- src/components/global/Playground/index.tsx
+++ src/components/global/Playground/index.tsx
@@ -224,7 +224,18 @@ export default function Playground({
           </svg>
         );
       case 'vue':
-        break;
+        return (
+          <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 512 512">
+            <polygon
+              fill="#33475A"
+              points="256 144.03 200.51 47.92 121.08 47.92 256 281.61 390.92 47.92 311.49 47.92 256 144.03"
+            />
+            <polygon
+              fill="#3FB27F"
+              points="409.4 47.92 256 313.61 102.6 47.92 15.74 47.92 256 464.08 496.26 47.92 409.4 47.92"
+            />
+          </svg>
+        );
     }
   }
 
