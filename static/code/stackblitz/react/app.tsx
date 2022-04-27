2ef12e20008def93c7535f55ff31f2b917bf05c0
Sean Perkins <sean@ionic.io>
2022-04-27 14:38:01 -0400

    fix(stackblitz/react): global classes in example

--- static/code/stackblitz/react/app.tsx
+++ static/code/stackblitz/react/app.tsx
@@ -6,6 +6,12 @@ import '@ionic/react/css/core.css';
 import '@ionic/react/css/normalize.css';
 import '@ionic/react/css/structure.css';
 import '@ionic/react/css/typography.css';
+/* Optional CSS utils that can be commented out */
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
 
 setupIonicReact();
 
