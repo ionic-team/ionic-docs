b2a18dd3425bfa9c895e17a686aa0ce0465ff216
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2022-05-10 11:34:10 -0400

    fix(playground): vanilla JS stackblitz loads core styles (#2330)

--- static/code/stackblitz/html/index.ts
+++ static/code/stackblitz/html/index.ts
@@ -2,6 +2,21 @@ import { defineCustomElements } from '@ionic/core/loader';
 
 import { pickerController } from '@ionic/core';
 
+/* Core CSS required for Ionic components to work properly */
+import '@ionic/core/css/core.css';
+
+/* Basic CSS for apps built with Ionic */
+import '@ionic/core/css/normalize.css';
+import '@ionic/core/css/structure.css';
+import '@ionic/core/css/typography.css';
+
+/* Optional CSS utils that can be commented out */
+import '@ionic/core/css/padding.css';
+import '@ionic/core/css/float-elements.css';
+import '@ionic/core/css/text-alignment.css';
+import '@ionic/core/css/text-transformation.css';
+import '@ionic/core/css/flex-utils.css';
+
 defineCustomElements();
 
 (window as any).pickerController = pickerController;
