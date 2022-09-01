4766c1f95f6bd2355173a3aff3d5abac73ecec0a
Liam DeBeasi <liamdebeasi@icloud.com>
2022-09-01 09:47:06 -0400

    Revert "fix(playground): use className instead of class"
    This reverts commit 62b628bbe5846d6f2502d015b5d70af94f8276d5.

--- src/components/global/Playground/icons/IconDots.tsx
+++ src/components/global/Playground/icons/IconDots.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import './IconDots.css';
 
 export const IconDots = () => (
-  <div className="icon__dots">
+  <div class="icon__dots">
     <svg viewBox="0 0 64 64">
       <circle transform="translate(32,32)" r="6"></circle>
     </svg>
