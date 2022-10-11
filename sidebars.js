1e8171376f07cf81596690999c8f605e1e4eafbb
Brandy Carney <brandy@ionic.io>
2022-10-11 12:06:50 -0400

    fix url

--- sidebars.js
+++ sidebars.js
@@ -30,7 +30,7 @@ module.exports = {
         {
           type: 'link',
           label: 'Responsive Grid',
-          href: './api/grid',
+          href: '/docs/api/grid',
         },
         'layout/global-stylesheets',
         'layout/css-utilities'
