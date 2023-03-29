a5ef14ca60e3fe13acce3add7563f5c2b534d6e2
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-03-29 14:42:08 -0400

    chore(): rdo not call i18n scripy

--- scripts/i18n.sh
+++ scripts/i18n.sh
@@ -6,5 +6,4 @@ if [ "${VERCEL_ENV:-preview}" == "production" ]; then
     mkdir -p i18n/ja/docusaurus-plugin-content-docs/version-v6/
     curl -fsSL https://github.com/ionic-team/ionic-docs/archive/refs/heads/translation/jp.tar.gz |
       tar -zxf - -C i18n/ja/docusaurus-plugin-content-docs/version-v6/ --strip-components 2 ionic-docs-translation-jp/docs
-    node scripts/api-ja.js
 fi
