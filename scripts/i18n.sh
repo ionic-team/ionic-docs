9f6282bef10e17ce84ddb33610e68ada834fbe4e
榊原昌彦 <sakakibara@rdlabo.jp>
2023-04-06 15:16:06 +0900

    fix(jp): set the v7 document version to v7

--- scripts/i18n.sh
+++ scripts/i18n.sh
@@ -2,7 +2,7 @@
 set -o errexit
 set -o nounset
 
-mkdir -p i18n/ja/docusaurus-plugin-content-docs/version-v6/
+mkdir -p i18n/ja/docusaurus-plugin-content-docs/current/
 curl -fsSL https://github.com/ionic-team/ionic-docs/archive/refs/heads/translation/jp.tar.gz |
-  tar -zxf - -C i18n/ja/docusaurus-plugin-content-docs/version-v6/ --strip-components 2 ionic-docs-translation-jp/docs
+  tar -zxf - -C i18n/ja/docusaurus-plugin-content-docs/current/ --strip-components 2 ionic-docs-translation-jp/docs
 node scripts/api-ja.js
