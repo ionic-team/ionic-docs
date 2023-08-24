e4d6e729063c34b89e3e5187afba2578c9e36bfd
Shawn Taylor <shawn@ionic.io>
2023-08-24 15:47:59 -0400

    Conditional prompting

--- _templates/playground/new/index.js
+++ _templates/playground/new/index.js
@@ -1,5 +1,22 @@
 const changeCase = require('change-case');
 
+const componentPrompt = {
+  type: 'input',
+  name: 'name',
+  message: 'Which component is this playground for?',
+  initial: 'ion-button',
+  validate(value) {
+    return value.match(/^ion-[a-z/-]*[a-z]+$/) ? true : "Component name must be kebab-case and begin with 'ion-'";
+  },
+};
+
+const nonComponentPrompt = {
+  type: 'input',
+  name: 'name',
+  message: 'What is this playground for?',
+  initial: 'animations',
+};
+
 // see types of prompts:
 // https://github.com/enquirer/enquirer/tree/master/examples
 //
@@ -8,59 +25,60 @@ module.exports = {
     return inquirer
       .prompt([
         {
-          type: 'input',
-          name: 'name',
-          message: 'Which component is this playground for?',
-          initial: 'ion-button',
-          validate(value) {
-            return value.match(/^ion-[a-z/-]*[a-z]+$/)
-              ? true
-              : "Component name must be kebab-case and begin with 'ion-'";
-          },
-        },
-        {
-          type: 'input',
-          name: 'path',
-          message: 'What should the playground path be?',
-          hint: 'e.g. `basic` or `theming/colors`',
-          validate(value) {
-            return value.match(/^[a-z]+[a-z/-]*[a-z]+$/)
-              ? true
-              : "Path should begin and end with a letter and only contain lowercase letters, '-', or '/'";
-          },
-        },
-        {
-          type: 'select',
-          name: 'version',
-          message: 'Select the Ionic Framework version for the playground',
-          initial: '7',
-          choices: ['6', '7'],
-        },
-        {
           type: 'toggle',
-          name: 'css',
-          message: 'Generate custom CSS files?',
-          enabled: 'Yes',
-          disabled: 'No',
-        },
-        {
-          type: 'toggle',
-          name: 'angular_ts',
-          message: 'Generate an Angular TypeScript file?',
-          enabled: 'Yes',
-          disabled: 'No',
+          name: 'is_component',
+          message: 'Is this playground for a component?',
+          initial: true,
         },
       ])
       .then((answers) => {
-        const componentName = changeCase.pascal(answers.path.split('/').pop());
-        console.log(
-          `\nTo use this component in a docs markdown file, include\nthe following:\n\n## ${componentName}\n\nimport ${componentName} from '@site/static/usage/v7/${answers.name.replace(
-            'ion-',
-            ''
-          )}/${answers.path}/index.md';\n\n<${componentName} />\n`
-        );
+        return inquirer
+          .prompt([
+            answers.is_component ? componentPrompt : nonComponentPrompt,
+            {
+              type: 'input',
+              name: 'path',
+              message: 'What should the playground path be?',
+              hint: 'e.g. `basic` or `theming/colors`',
+              validate(value) {
+                return value.match(/^[a-z]+[a-z/-]*[a-z]+$/)
+                  ? true
+                  : "Path should begin and end with a letter and only contain lowercase letters, '-', or '/'";
+              },
+            },
+            {
+              type: 'select',
+              name: 'version',
+              message: 'Select the Ionic Framework version for the playground',
+              initial: '7',
+              choices: ['6', '7'],
+            },
+            {
+              type: 'toggle',
+              name: 'css',
+              message: 'Generate custom CSS files?',
+              enabled: 'Yes',
+              disabled: 'No',
+            },
+            {
+              type: 'toggle',
+              name: 'angular_ts',
+              message: 'Generate an Angular TypeScript file?',
+              enabled: 'Yes',
+              disabled: 'No',
+            },
+          ])
+          .then((answers) => {
+            const componentName = changeCase.pascal(answers.path.split('/').pop());
+            console.log(
+              `\nTo use this component in a docs markdown file, include\nthe following:\n\n## ${componentName}\n\nimport ${componentName} from '@site/static/usage/v7/${answers.name.replace(
+                'ion-',
+                ''
+              )}/${answers.path}/index.md';\n\n<${componentName} />\n`
+            );
 
-        return answers;
+            return answers;
+          });
       });
   },
 };
