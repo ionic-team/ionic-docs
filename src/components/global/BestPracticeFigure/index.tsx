9d34ccfad8351cbc41b8579ca75d94fe4a1447f7
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-09-20 18:04:19 -0400

    add docs

--- src/components/global/BestPracticeFigure/index.tsx
+++ src/components/global/BestPracticeFigure/index.tsx
@@ -2,6 +2,50 @@ import React from 'react';
 
 import './best-practice-figure.css';
 
+/**
+ Usage:
+
+ import BestPracticeFigure from '@components/global/BestPracticeFigure';
+
+ <BestPracticeFigure
+   text="..."
+   doText="..."
+   dontText="..."
+   doImage={<img alt="..." src={useBaseUrl(...)} />}
+   dontImage={<img alt="..." src={useBaseUrl(...)} />}
+ />
+
+ All images must have alt text for screen readers.
+
+ Custom HTML can also be passed to any of the text properties by using curly braces:
+
+ text={<>My text with a <a href="#">link</a></>}
+
+ Markdown such as backticks will not be processed when using curly braces.
+ The <code> tag can be used instead:
+
+ text={<>The <code>button</code> property</>}
+
+ This component also supports a caution option. Typically you should use
+ either a "Don't" image or a "Caution" image but not both.
+
+ <BestPracticeFigure
+    text="..."
+    doText="..."
+    cautionText="..."
+    doImage={<img alt="..." src={useBaseUrl(...)} />}
+    cautionImage={<img alt="..." src={useBaseUrl(...)} />}
+  />
+
+  @prop text - Text that describes the figure as a whole
+  @prop doText - Text that describes a best practice
+  @prop dontText - Text that describes an anti-pattern
+  @prop cautionText - Text that describes something that could be an anti-pattern based on use case
+  @prop doImage - Image associated with doText
+  @prop dontImage - Image associated with dontText
+  @prop cautionImage - Image associated with cautionText
+ */
+
 export default function BestPracticeFigure({ text, doText, dontText, cautionText, doImage, dontImage, cautionImage }) {
   return (
     <div className="best-practice__container">
