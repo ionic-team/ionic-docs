fbb036134e79143d664a9df8fa200a3d90b77e89
Maria Hutt <maria@ionic.io>
2023-11-01 14:55:59 -0700

    fix(locales): add redirect links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,8 +11,7 @@ module.exports = {
   tagline:
     'Ionic is the app platform for web developers. Build amazing mobile, web, and desktop apps all with one shared code base and open web standards',
   url: 'https://ionicframework.com',
-  baseUrl: `${BASE_URL}`,
-  // trailingSlash: true,
+  baseUrl: `${BASE_URL}/`,
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'ja'],
