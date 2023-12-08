443340e471b978b5f488b052b13246b3df616533
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-12-08 12:52:38 -0500

    stackblitz works with v8

---
+++ static/code/stackblitz/v8/html/index.ts
@@ -0,0 +1,32 @@
+import { defineCustomElements } from '@ionic/core/loader';
+
+import { createAnimation, createGesture, loadingController, menuController, modalController, pickerController, toastController } from '@ionic/core';
+
+/* Core CSS required for Ionic components to work properly */
+import '@ionic/core/css/core.css';
+
+/* Basic CSS for apps built with Ionic */
+import '@ionic/core/css/normalize.css';
+import '@ionic/core/css/structure.css';
+import '@ionic/core/css/typography.css';
+
+/* Optional CSS utils that can be commented out */
+import '@ionic/core/css/padding.css';
+import '@ionic/core/css/float-elements.css';
+import '@ionic/core/css/text-alignment.css';
+import '@ionic/core/css/text-transformation.css';
+import '@ionic/core/css/flex-utils.css';
+import '@ionic/core/css/display.css';
+
+/* Theme variables */
+import './theme/variables.css';
+
+defineCustomElements();
+
+(window as any).loadingController = loadingController;
+(window as any).menuController = menuController;
+(window as any).modalController = modalController;
+(window as any).pickerController = pickerController;
+(window as any).toastController = toastController;
+(window as any).createAnimation = createAnimation;
+(window as any).createGesture = createGesture;
