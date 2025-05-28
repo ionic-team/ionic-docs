21aaf53ae8e0b3f4efb5f05b54b3032d42f7aa62
Brandy Smith <6577830+brandyscarney@users.noreply.github.com>
2025-05-28 17:21:00 -0400

    chore(): latest dev build

--- plugins/docusaurus-plugin-ionic-component-api/index.js
+++ plugins/docusaurus-plugin-ionic-component-api/index.js
@@ -51,7 +51,7 @@ module.exports = function (context, options) {
       }
 
       // TODO: revert this before merging
-      let npmTag = '8.5.7-dev.11748379940.12698faf';
+      let npmTag = '8.5.7-dev.11748466845.12de1519';
       // Latest version
       await generateMarkdownForVersion(currentVersion.path || currentVersion.label, npmTag, true);
 
