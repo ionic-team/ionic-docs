aaadddabb19520ea1a3e6ea70c049ec9807b0b0e
Brandy Smith <6577830+brandyscarney@users.noreply.github.com>
2025-06-03 18:00:08 -0400

    chore(): hardcode dev build

--- plugins/docusaurus-plugin-ionic-component-api/index.js
+++ plugins/docusaurus-plugin-ionic-component-api/index.js
@@ -50,12 +50,8 @@ module.exports = function (context, options) {
         await generateMarkdownForVersion(version, npmTag, false);
       }
 
-      let npmTag = 'latest';
-      if (currentVersion.banner === 'unreleased') {
-        npmTag = 'next';
-      } else if (currentVersion.path !== undefined) {
-        npmTag = currentVersion.path.slice(1);
-      }
+      // TODO: revert this before merging
+      let npmTag = '8.5.7-dev.11748545890.14d6a067';
       // Latest version
       await generateMarkdownForVersion(currentVersion.path || currentVersion.label, npmTag, true);
 
