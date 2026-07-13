474bb8e795073807ca982dcda3b663340fd1a2ae
OS-jacobbell <228905018+OS-jacobbell@users.noreply.github.com>
2026-07-13 14:36:17 -0600

    update imports in playground

--- src/components/global/Playground/stackblitz.utils.spec.ts
+++ src/components/global/Playground/stackblitz.utils.spec.ts
@@ -314,7 +314,7 @@ describe('stackblitz.utils', () => {
         'angular/package.json': '{}',
         'angular/main.ts': `import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
-import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
@@ -351,7 +351,7 @@ bootstrapApplication(AppComponent, {
       global.fetch = createMockFetch({
         'angular/package.json': '{}',
         'angular/main.ts': `import { bootstrapApplication } from '@angular/platform-browser';
-import { provideIonicAngular } from '@ionic/angular/standalone';
+import { provideIonicAngular } from '@ionic/angular';
 
 bootstrapApplication(AppComponent, {
   providers: [
